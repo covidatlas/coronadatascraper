9a792db962c34320184aa32f031fab44f4c91b6b
Larry Davis <lazdnet@gmail.com>
2020-03-13 22:49:21 -0700

    feat: add China and Australia region data

--- scrapers.js
+++ scrapers.js
@@ -1,6 +1,7 @@
 import * as fetch from './lib/fetch.js';
 import * as parse from './lib/parse.js';
 import * as transform from './lib/transform.js';
+import * as rules from './lib/rules.js';
 
 /*
   Each scraper must return the following object or an array of the following objects:
@@ -43,13 +44,28 @@ let scrapers = [
     }
   },
   {
-    // Get country data only from JHU
     url: 'https://github.com/CSSEGISandData/COVID-19',
     _urls: {
       cases: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
       deaths: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
       recovered: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
     },
+    _reject: [
+      {
+        'Province/State': 'From Diamond Princess'
+      }
+    ],
+    _accept: [
+      {
+        'Province/State': ''
+      },
+      {
+        'Country/Region': 'China'
+      },
+      {
+        'Country/Region': 'Australia'
+      }
+    ],
     scraper: async function() {
       let cases = await fetch.csv(this._urls.cases);
       let deaths = await fetch.csv(this._urls.deaths);
@@ -58,9 +74,10 @@ let scrapers = [
       let countries = [];
       let latestDate = Object.keys(cases[0]).pop();
       for (let index = 0; index < cases.length; index++) {
-        if (!cases[index]['Province/State']) {
+        if (rules.isAcceptable(cases[index], this._accept, this._reject)) {
           countries.push({
             country: parse.string(cases[index]['Country/Region']),
+            state: parse.string(cases[index]['Province/State']),
             cases: parse.number(cases[index][latestDate]),
             recovered: parse.number(recovered[index][latestDate]),
             deaths: parse.number(deaths[index][latestDate])
