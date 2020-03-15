fcc7b116da6ffa993cf8c0affe64234fc4c7f47b
Steve Wallace <logosmonkey@gmail.com>
2020-03-14 20:47:52 -0400

    feat: add Connecticut, USA counties (#36)
    fix: Orange County, CA data

--- scrapers.js
+++ scrapers.js
@@ -974,7 +974,7 @@ let scrapers = [
 
       return {
         cases: parse.number(
-          $('td:contains("Confirmed Cases")')
+          $('td:contains("Cases")')
             .next()
             .text()
         ),
@@ -1195,6 +1195,31 @@ let scrapers = [
           cases: parse.number(splitCounty[1])
         });
       });
+      return counties;
+    }
+  },
+  {
+    state: 'CT',
+    country: 'USA',
+    url: 'https://portal.ct.gov/Coronavirus',
+    scraper: async function() {
+      let counties = [];
+      let $ = await fetch.page(this.url);
+      let $lis = $('span:contains("Latest COVID-19 Testing Data in Connecticut")')
+                  .nextAll('ul')
+                  .first()
+                  .find('li');
+                
+      $lis.each((index, li) => {
+        if(index < 1) {
+          return
+        }
+        let countyData = $(li).text().split(': ');
+           counties.push({
+            county: parse.string(countyData[0]),
+            cases: parse.number(countyData[1])
+          });
+      })      
       return counties;
     }
   }
