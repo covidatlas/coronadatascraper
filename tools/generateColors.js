955fe665e19eb1a1771567b2bdd8934bcd22c297
Nate Baldwin <nbaldwin@adobe.com>
2020-03-17 13:25:03 -0600

    adding leonardo for generating colors

---
+++ tools/generateColors.js
@@ -0,0 +1,66 @@
+import { generateAdaptiveTheme } from '@adobe/leonardo-contrast-colors';
+
+// returns theme colors as JSON
+function generateColors() {
+  let cds_theme = generateAdaptiveTheme({
+    baseScale: "Gray",
+    colorScales: [
+      {
+        name: "Gray",
+        colorKeys: [
+          "#355166"
+        ],
+        colorspace: "RGB",
+        ratios: [
+          -1.2,
+          -1.05,
+          1,
+          1.1,
+          1.2,
+          1.5,
+          1.93,
+          3.01,
+          4.54,
+          8,
+          12.81
+        ]
+      },
+      {
+        name: "Blue",
+        colorKeys: [
+          "#21385e",
+          "#829800",
+          "#007e60",
+          "#efef00"
+        ],
+        colorspace: "LCH",
+        ratios: [
+          3.2,
+          4.8,
+          7.4,
+          11.7
+        ]
+      }
+    ]
+  });
+
+  let cds_theme_light = cds_theme(97);
+
+  let varPrefix = '--';
+
+  // Iterate each color object
+  for (let i = 0; i < cds_theme_light.length; i++) {
+    // Iterate each value object within each color object
+    for(let j = 0; j < cds_theme_light[i].values.length; j++) {
+      // output "name" of color and prefix
+      let key = cds_theme_light[i].values[j].name;
+      let prop = varPrefix.concat(key);
+      // output value of color
+      let value = cds_theme_light[i].values[j].value;
+      // create CSS property with name and value
+      document.documentElement.style
+        .setProperty(prop, value);
+    }
+  }
+}
+generateColors();
