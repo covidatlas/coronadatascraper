bf72c06a3b47985548804153d8f833c29e231b45
Larry Davis <lazdnet@gmail.com>
2020-03-16 17:30:59 -0700

    fix: use cli lib

--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 import path from 'path';
-import yargs from 'yargs';
 import generate from './index.js';
 import * as fs from './lib/fs.js';
 import * as stringify from './lib/stringify.js';
+import argv from './lib/cliArgs.js';
 
 async function writeData({ locations }) {
   let date = process.env['SCRAPE_DATE'] ?  '-' + process.env['SCRAPE_DATE'] : '';
@@ -16,30 +16,5 @@ async function writeData({ locations }) {
   return { locations };
 }
 
-const argv = yargs
-  .option('date', {
-    alias: 'd',
-    description: 'Generate data for the provided date in YYYY-M-D format',
-    type: 'string',
-  })
-  .option('only', {
-    alias: 'o',
-    description: 'Scrape only the location provided by full name, i.e City, County, State, Country',
-    type: 'string',
-  })
-  .option('skip', {
-    alias: 's',
-    description: 'Skip the location provided by full name, i.e City, County, State, Country',
-    type: 'string',
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-let rules = {
-  only: argv.only,
-  skip: argv.skip
-};
-
-generate(argv.date, rules)
+generate(argv.date, argv)
   .then(writeData);
