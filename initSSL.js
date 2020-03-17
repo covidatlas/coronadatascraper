11f1dc27c467754d6e5d62d7ff9c2cd78903f06e
Cameron Chamberlain <git@camjc.com>
2020-03-17 22:34:22 +1100

    [patch] fix stray import where extension seems required

--- initSSL.js
+++ initSSL.js
@@ -1,4 +1,5 @@
-import rootCas from 'ssl-root-cas/latest';
+// eslint-disable-next-line import/extensions
+import rootCas from 'ssl-root-cas/latest.js';
 
 // SSL Root Cas requires us to run this function once to fetch
 // certificates
