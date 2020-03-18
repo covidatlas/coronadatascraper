ea305b55b56dd20456ff10ebc65c26997f96b494
jeffchenoweth <jeffrey.chenoweth@gmail.com>
2020-03-17 22:25:11 -0400

    Update to resolve pullrequest comments

--- scrapers.js
+++ scrapers.js
@@ -43,21 +43,22 @@ const scrapers = [
   {
     state: 'IL',
     country: 'USA',
+    priority: 1,
     url: 'http://www.dph.illinois.gov/sites/default/files/COVID19/COVID19CountyResults.json',
     async scraper() {
       const data = await fetch.json(this.url);
 
       const counties = [];
-      // do stuff
+      
       for (const county of data.characteristics_by_county.values) {
         counties.push({
-          county: `${county.County} County`,
+          county: transform.addCounty(county.County),
           cases: county.confirmed_cases,
           tested: county.total_tested
         });
       }
+      counties.push(transform.sumData(counties));
 
-      // await browser.close();
       return counties;
     }
   },
