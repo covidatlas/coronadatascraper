123f67c80de7e936cdf9ce78a94ecfd552a83a51
Samir Rayani <samir.rayani@gmail.com>
2020-03-19 16:37:42 -0400

    remove redundant await

--- scrapers2.js
+++ scrapers2.js
@@ -1,12 +1,6 @@
 import fastGlob from 'fast-glob';
 
 export default async () => {
-	const filePaths = await fastGlob(['./scrapers/**/*.js']);
-	return await Promise.all(
-		filePaths.map(filePath => import(filePath)))
-			.then(modules => [
-				...modules.map(module => module.default)
-		]);
+  const filePaths = await fastGlob(['./scrapers/**/*.js']);
+  return Promise.all(filePaths.map(filePath => import(filePath))).then(modules => [...modules.map(module => module.default)]);
 };
-
-// export { loadScrapers }
