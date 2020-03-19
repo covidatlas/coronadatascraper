3ed902139048f708dd73a000198e25ea2542c9e3
jbencina <jbencina@linkedin.com>
2020-03-19 08:52:32 -0700

    Updated Mono County, CA to include tests

--- scrapers.js
+++ scrapers.js
@@ -1816,20 +1816,48 @@ const scrapers = [
     state: 'CA',
     country: 'USA',
     url: 'https://monocovid19-monomammoth.hub.arcgis.com/',
-    async scraper() {
-      const $ = await fetch.headless(this.url);
+    scraper: {
+      '0': async function() {
+        const $ = await fetch.headless(this.url);
 
-      const cases = parse.number(
-        $('h4:contains("POSITIVE")')
-          .first()
-          .parent()
-          .next('h3')
-          .text()
-      );
+        const cases = parse.number(
+          $('h4:contains("POSITIVE")')
+            .first()
+            .parent()
+            .next('h3')
+            .text()
+        );
 
-      return {
-        cases
-      };
+        return {
+          cases
+        };
+      },
+      '2020-3-19': async function() {
+        const $ = await fetch.headless(this.url);
+
+        const cases = parse.number(
+          $('h4:contains("POSITIVECASES")')
+            .first()
+            .parent()
+            .find('h3')
+            .first()
+            .text()
+        );
+
+        const tested = parse.number(
+          $('h4:contains("TESTSGIVEN")')
+            .first()
+            .parent()
+            .find('h3')
+            .first()
+            .text()
+        );
+
+        return {
+          cases,
+          tested
+        };
+      }
     }
   },
   {
