6179ba638c042528fb1c463389f0f594d5b9d2ff
Samir Rayani <samir.rayani@gmail.com>
2020-03-19 17:20:11 -0400

    remove comment block!

--- scrapers/CHE/zurich.js
+++ scrapers/CHE/zurich.js
@@ -2,22 +2,6 @@ import * as fetch from '../../lib/fetch.js';
 import * as parse from '../../lib/parse.js';
 import * as datetime from '../../lib/datetime.js';
 
-/*
-  Each scraper must return the following object or an array of the following objects:
-
-  {
-    city: String†,
-    county: String†,   // County or region name, complete with "County" or "Parish" at the end
-    country: String†,  // ISO 3166-1 alpha-3 country code
-    cases: Integer,    // Confirmed cases (including presumptive)
-    deaths: Integer,
-    recovered: Integer,
-    tested: Integer
-  }
-
-  † Optional, not required if provided in the main scraper definition
-*/
-
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
 
