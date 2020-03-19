5090099331a93559a217179cf6b40684fe2cddaf
jbencina <jbencina@linkedin.com>
2020-03-18 17:39:29 -0700

    Fix Del Norte County, CA

--- scrapers.js
+++ scrapers.js
@@ -1568,43 +1568,67 @@ const scrapers = [
     state: 'CA',
     country: 'USA',
     url: 'http://www.co.del-norte.ca.us/departments/health-human-services/public-health',
-    async scraper() {
-      const $ = await fetch.page(this.url);
+    scraper: {
+      '0': async function() {
+        const $ = await fetch.page(this.url);
 
-      const cases = parse.number(
-        $('font:contains("Number of Confirmed Cases")')
-          .first()
-          .text()
-          .match(/(\d+)/)[1]
-      );
+        const cases = parse.number(
+          $('font:contains("Number of Confirmed Cases")')
+            .first()
+            .text()
+            .match(/(\d+)/)[1]
+        );
 
-      const pui = parse.number(
-        $('font:contains("Number of Persons Under Investigation")')
-          .first()
-          .text()
-          .match(/(\d+)/)[1]
-      );
+        const pui = parse.number(
+          $('font:contains("Number of Persons Under Investigation")')
+            .first()
+            .text()
+            .match(/(\d+)/)[1]
+        );
 
-      const pending = parse.number(
-        $('font:contains("Number of Specimens with Results Pending")')
-          .first()
-          .text()
-          .match(/(\d+)/)[1]
-      );
+        const pending = parse.number(
+          $('font:contains("Number of Specimens with Results Pending")')
+            .first()
+            .text()
+            .match(/(\d+)/)[1]
+        );
 
-      const negative = parse.number(
-        $('font:contains("Number of Negative Tests")')
-          .first()
-          .text()
-          .match(/(\d+)/)[1]
-      );
+        const negative = parse.number(
+          $('font:contains("Number of Negative Tests")')
+            .first()
+            .text()
+            .match(/(\d+)/)[1]
+        );
 
-      const tested = pui + pending + negative;
+        const tested = pui + pending + negative;
 
-      return {
-        cases,
-        tested
-      };
+        return {
+          cases,
+          tested
+        };
+      },
+      '2020-3-18': async function() {
+        const $ = await fetch.page(this.url);
+
+        const cases = parse.number(
+          $('font:contains("Number of Positive")')
+            .first()
+            .text()
+            .match(/(\d+)$/)[1]
+        );
+
+        const tested = parse.number(
+          $('font:contains("Number of Tests Administered")')
+            .first()
+            .text()
+            .match(/(\d+)/)[1]
+        );
+
+        return {
+          cases,
+          tested
+        };
+      }
     }
   },
   {
