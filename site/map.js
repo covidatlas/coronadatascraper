407e7b025868373c2d46470d143692b70d78ae3c
Nate Baldwin <nbaldwin@adobe.com>
2020-03-20 13:29:34 -0600

    added new color and legend functions

--- site/map.js
+++ site/map.js
@@ -24,6 +24,7 @@ const outlineColor = 'rgba(0, 0, 0, 0.3)';
 const choroplethColors = {
   stoplight: ['#eeffcd', '#b4ffa5', '#ffff00', '#ff7f00', '#ff0000'],
   yellowOrangePurple: ['#faffef', '#f3fac1', '#f6f191', '#ffe15d', '#fec327', '#ff9b00', '#fe7000', '#fa4d13', '#c52155', '#842e79'],
+  yellowOrangeRed: ['#fff5bd', '#fce289', '#fcce54', '#ffb601', '#ff8f00', '#fd6100', '#e03d19', '#b52725', '#801f28', '#4b1a21'],
   heat: ['#FFFFFF', '#ffff5e', '#ffe70c', '#fead0a', '#fd6f08', '#fd2907', '#fd0407'],
   peach: ['rgb(253,222,166)', 'rgb(255,188,134)', 'rgb(249,152,133)', 'rgb(232,110,129)', 'rgb(224,88,136)'],
   pink: ['rgb(255, 244, 221)', 'rgb(255, 221, 215)', 'rgb(255, 197, 210)', 'rgb(254, 174, 203)', 'rgb(250, 150, 196)', 'rgb(245, 126, 189)', 'rgb(239, 100, 181)', 'rgb(232, 70, 173)', 'rgb(210, 56, 161)', 'rgb(187, 46, 150)', 'rgb(163, 36, 140)', 'rgb(138, 27, 131)', 'rgb(113, 22, 124)', 'rgb(86, 15, 116)', 'rgb(55, 11, 110)', 'rgb(0, 9, 104)'],
@@ -360,6 +361,58 @@ function showMap() {
   loadData('locations.json', 'locations');
   loadData('timeseries.json', 'timeseries');
   loadData('features.json', 'features');
+}
+
+window.createLegend = createLegend;
+function createLegend(min, max) {
+  const base = document.getElementById('map');
+  const container = document.createElement('div');
+  const containerId = 'mapLegend';
+  container.id = containerId;
+
+  const heading = document.createElement('span');
+  heading.className = 'spectrum-Heading spectrum-Heading--XXXS';
+  heading.innerHTML = 'Cases by population';
+  container.appendChild(heading);
+
+  base.appendChild(container);
+  ramp(choroplethColors.yellowOrangeRed, 300, containerId);
+
+  // Correct value of max percent so that it's easier to parse
+  const lowestPercent = min * 100;
+  const worstPercent = max * 100;
+
+  const scaleText = document.createElement('span');
+  scaleText.className = 'mapLegend-scaleLabels';
+  const startText = document.createElement('span');
+  startText.className = 'spectrum-Body spectrum-Body--XS';
+  startText.innerHTML = `${lowestPercent}%`;
+  const endText = document.createElement('span');
+  endText.className = 'spectrum-Body spectrum-Body--XS';
+  endText.innerHTML = `${worstPercent}%`;
+  scaleText.appendChild(startText);
+  scaleText.appendChild(endText);
+  container.appendChild(scaleText);
+}
+
+function ramp(color, n, containerId) {
+  const id = '#'.concat(containerId);
+  const base = d3.select(id);
+  const canvas = base
+    .append('canvas')
+    .attr('width', n)
+    .attr('height', '16px');
+  const context = canvas.node().getContext('2d');
+
+  canvas.style.width = '300px';
+  canvas.style.height = '16px';
+  canvas.style.imageRendering = 'pixelated';
+
+  for (let i = 0; i < n; ++i) {
+    context.fillStyle = fill(i / (n - 1));
+    context.fillRect(i, 0, 1, 32);
+  }
+  return canvas;
 }
 
 export default showMap;
