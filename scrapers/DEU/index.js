d6f35bde1553b40ef0e1bd1e0fe6cd7a60a11dad
Ole Kröger <o.kroeger@opensourc.es>
2020-03-20 14:29:00 +0100

    county level data for Germany

--- scrapers/DEU/index.js
+++ scrapers/DEU/index.js
@@ -1,34 +1,33 @@
 import * as fetch from '../../lib/fetch.js';
+import * as parse from '../../lib/parse.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
 
 const scraper = {
   country: 'DEU',
-  url: 'https://covid19-germany.appspot.com/now',
+  url: 'https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.csv',
   type: 'json',
-  curators: [
-    {
-      name: 'Dr. Jan-Philip Gehrcke',
-      email: 'jgehrcke@googlemail.com'
-    }
-  ],
+  aggregate: 'county',
   sources: [
     {
-      name: 'Berliner Morgenpost',
-      description: 'Aggregated data from individual ministries of health in Germany'
+      name: 'RKI county data',
+      description: 'RKI county data as an arcgis dataset'
     }
   ],
   async scraper() {
-    const data = await fetch.json(this.url);
-    return {
-      country: 'DEU',
-      cases: data.current_totals.cases,
-      deaths: data.current_totals.deaths,
-      recovered: data.current_totals.recovered,
-      coordinates: [9, 51],
-      population: 83 * 10 ** 6
-    };
+    const data = await fetch.csv(this.url);
+    const counties = [];
+    for (const county of data) {
+      counties.push({
+        county: parse.string(county.GEN),
+        state: parse.string(county.BL),
+        cases: parse.number(county.cases),
+        deaths: parse.number(county.deaths),
+        population: parse.number(county.EWZ)
+      });
+    }
+    return counties;
   }
 };
 
