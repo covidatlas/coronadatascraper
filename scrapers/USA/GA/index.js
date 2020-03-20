223be03d4c6ba606de00a91a6d8bc26af5218a27
Larry Davis <lazdnet@gmail.com>
2020-03-20 16:57:44 -0700

    feat: add rollup

--- scrapers/USA/GA/index.js
+++ scrapers/USA/GA/index.js
@@ -1,6 +1,7 @@
 import * as fetch from '../../../lib/fetch.js';
 import * as parse from '../../../lib/parse.js';
 import * as geography from '../../../lib/geography.js';
+import * as transform from '../../../lib/transform.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -21,6 +22,8 @@ const scraper = {
       const cases = parse.number($tr.find('td:last-child').text());
       counties.push({ county, cases });
     });
+
+    counties.push(transform.sumData(counties));
 
     return counties;
   }
