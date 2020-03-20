d96f6dd297353c2cd26b52ffef733417f1847d0b
Larry Davis <lazdnet@gmail.com>
2020-03-20 01:11:29 -0700

    build: use rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import json from '@rollup/plugin-json';
+import serve from 'rollup-plugin-serve';
+import copy from 'rollup-plugin-copy';
+import postcss from 'rollup-plugin-postcss';
+import postcssNested from 'postcss-nested';
+import postcssImport from 'postcss-import';
+
+export default {
+  input: 'site/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'iife',
+    sourcemap: true
+  },
+  plugins: [
+    json(),
+    serve('dist'),
+    copy({
+      targets: [
+        { src: ['site/*', '!index.js', '!style.css', '!icons/style.css'], dest: 'dist/' },
+      ]
+    }),
+    postcss({
+      plugins: [
+        postcssImport(),
+        postcssNested()
+      ]
+    })
+  ]
+};
