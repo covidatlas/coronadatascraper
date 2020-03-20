463a3c2298448bf976a897ea799a5d8defbf7f70
Jeffrey Chenoweth <jeffrey.chenoweth@gmail.com>
2020-03-20 15:19:12 -0400

    feat: add GA county data

---
+++ scrapers/USA/GA/index.js
@@ -0,0 +1,29 @@
+import * as fetch from '../../../lib/fetch.js';
+import * as parse from '../../../lib/parse.js';
+import * as transform from '../../../lib/transform.js';
+
+// Set county to this if you only have state data, but this isn't the entire state
+// const UNASSIGNED = '(unassigned)';
+
+const scraper = {
+  state: 'GA',
+  country: 'USA',
+  url: 'https://dph.georgia.gov/covid-19-daily-status-report',
+  type: 'table',
+  async scraper() {
+    const $ = await fetch.page(this.url);
+    let counties = [];
+    let $table = $('table:contains(County):contains(Cases)');
+    let $trs = $table.find('tbody > tr');
+    $trs.each((index, tr) => {
+      const $tr = $(tr);
+      const county = transform.addCounty(parse.string($tr.find('td:first-child').text()));
+      const cases = parse.number($tr.find('td:last-child').text());
+      counties.push({county: county, cases: cases});
+    });
+
+    return counties;
+  }
+};
+
+export default scraper;
