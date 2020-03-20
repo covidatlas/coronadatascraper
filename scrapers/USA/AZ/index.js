a8cdec79237e08a4985b1f0c770465e3b4afae65
Paul Gauthier <github.com@paulg.org>
2020-03-19 23:19:10 -0700

    Pulled cases & deaths from the AZ tableau API

--- scrapers/USA/AZ/index.js
+++ scrapers/USA/AZ/index.js
@@ -1,13 +1,28 @@
+import * as fetch from '../../../lib/fetch.js';
+import * as parse from '../../../lib/parse.js';
+
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
 
 const scraper = {
   state: 'AZ',
   country: 'USA',
-  url: 'https://tableau.azdhs.gov/views/COVID-19Dashboard/COVID-19table?:isGuestRedirectFromVizportal=y&:embed=y',
-  async _scraper() {
-    const counties = [];
-    return counties;
+  url: 'https://tableau.azdhs.gov/views/COVID-19Dashboard/COVID-19table.csv?%3AisGuestRedirectFromVizportal=y&%3Aembed=y',
+  type: 'csv',
+  async scraper() {
+    let cases = 0;
+    let deaths = 0;
+
+    const data = await fetch.csv(this.url);
+    for (const row of data) {
+      if (row.Category === 'Total Cases') {
+        cases += parse.number(row.Positive);
+      } else if (row.Category === 'Total Deaths') {
+        deaths += parse.number(row.Positive);
+      }
+    }
+
+    return { cases, deaths };
   }
 };
 
