36c4889f602143b15cb76608e22972e114c9038e
Larry Davis <lazdnet@gmail.com>
2020-03-19 20:59:13 -0700

    fix: CT, USA

--- scrapers/USA/CT/index.js
+++ scrapers/USA/CT/index.js
@@ -1,5 +1,6 @@
 import * as fetch from '../../../lib/fetch.js';
 import * as parse from '../../../lib/parse.js';
+import * as transform from '../../../lib/transform.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -12,6 +13,7 @@ const scraper = {
   aggregate: 'county',
   scraper: {
     '0': async function() {
+      this.type = 'list';
       const counties = [];
       const $ = await fetch.page(this.url);
       const $lis = $('span:contains("Latest COVID-19 Testing Data in Connecticut")')
@@ -33,9 +35,10 @@ const scraper = {
       return counties;
     },
     '2020-03-18': async function() {
+      this.type = 'paragraph';
       const counties = [];
       const $ = await fetch.page(this.url);
-      const p = $('p:contains("Fairfield County:")')
+      const p = $(':contains("Fairfield County:")')
         .first()
         .text();
       const items = p.split('\n');
@@ -48,6 +51,27 @@ const scraper = {
           cases
         });
       }
+      return counties;
+    },
+    '2020-03-19': async function() {
+      this.type = 'table';
+      const counties = [];
+      const $ = await fetch.page(this.url);
+
+      const $table = $('td:contains("Fairfield County")').closest('table');
+      const $trs = $table.find('tbody > tr:not(:last-child)');
+      $trs.each((index, tr) => {
+        if (index < 2) {
+          return;
+        }
+        const $tr = $(tr);
+        const countyName = transform.addCounty(parse.string($tr.find('td:first-child').text()));
+        counties.push({
+          county: countyName,
+          cases: parse.number($tr.find('td:last-child').text())
+        });
+      });
+      counties.push(transform.sumData(counties));
       return counties;
     }
   }
