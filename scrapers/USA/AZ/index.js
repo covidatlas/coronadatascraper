afce61f0872a15bd6fe731de4b9b71b804b317bd
Paul Gauthier <github.com@paulg.org>
2020-03-20 00:37:06 -0700

    Added county level data for AZ

--- scrapers/USA/AZ/index.js
+++ scrapers/USA/AZ/index.js
@@ -1,5 +1,7 @@
+import csvParse from 'csv-parse/lib/sync';
 import * as fetch from '../../../lib/fetch.js';
 import * as parse from '../../../lib/parse.js';
+import * as transform from '../../../lib/transform.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -7,22 +9,42 @@ import * as parse from '../../../lib/parse.js';
 const scraper = {
   state: 'AZ',
   country: 'USA',
-  url: 'https://tableau.azdhs.gov/views/COVID-19Dashboard/COVID-19table.csv?%3AisGuestRedirectFromVizportal=y&%3Aembed=y',
+  url: 'https://tableau.azdhs.gov/views/COVID-19Dashboard/COVID-19table?%3AisGuestRedirectFromVizportal=y&%3Aembed=y',
   type: 'csv',
+  aggregate: 'county',
   async scraper() {
-    let cases = 0;
-    let deaths = 0;
+    // Get the Tableau chart
+    const $ = await fetch.headless(this.url);
 
-    const data = await fetch.csv(this.url);
+    // Pull out our session id from the json stuffed inside the textarea
+    const textArea = $('textarea#tsConfigContainer').text();
+    const j = JSON.parse(textArea);
+    const sessionId = j.sessionid;
+
+    // Fetch the magic URL with our current session ID
+    const url = `https://tableau.azdhs.gov/vizql/w/COVID-19Dashboard/v/COVID-19table/vud/sessions/${sessionId}/views/8275719771277684273_9753144220671897612?csv=true&summary=true`;
+
+    // Parse the tab separated values file that comes back
+    const tsv = await fetch.page(url);
+    const data = csvParse(tsv.text(), {
+      delimiter: '\t',
+      columns: true
+    });
+
+    const counties = [];
+
     for (const row of data) {
-      if (row.Category === 'Total Cases') {
-        cases += parse.number(row.Positive);
-      } else if (row.Category === 'Total Deaths') {
-        deaths += parse.number(row.Positive);
-      }
+      const county = transform.addCounty(row.County);
+      const cases = parse.number(row.Count);
+
+      counties.push({
+        county,
+        cases
+      });
     }
 
-    return { cases, deaths };
+    counties.push(transform.sumData(counties));
+    return counties;
   }
 };
 
