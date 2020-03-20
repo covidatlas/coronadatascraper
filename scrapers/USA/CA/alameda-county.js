192bdc28c17a73f59a6471c6c7ece5c8f7064f18
jbencina <jbencina@linkedin.com>
2020-03-19 20:43:47 -0700

    Add jbencina to maintainers of CA counties

--- scrapers/USA/CA/alameda-county.js
+++ scrapers/USA/CA/alameda-county.js
@@ -1,5 +1,6 @@
 import * as fetch from '../../../lib/fetch.js';
 import * as parse from '../../../lib/parse.js';
+import maintainers from '../../../lib/maintainers.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -11,6 +12,7 @@ const scraper = {
   url: 'http://www.acphd.org/2019-ncov.aspx',
   headless: true,
   type: 'paragraph',
+  maintainers: [maintainers.jbencina],
   async scraper() {
     const $ = await fetch.headless(this.url);
     const $el = $('p:contains("Positive Cases")');
--- scrapers/USA/CA/sonoma-county.js
+++ scrapers/USA/CA/sonoma-county.js
@@ -1,5 +1,6 @@
 import * as fetch from '../../../lib/fetch.js';
 import * as parse from '../../../lib/parse.js';
+import maintainers from '../../../lib/maintainers.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -8,6 +9,7 @@ const scraper = {
   county: 'Sonoma County',
   state: 'CA',
   country: 'USA',
+  maintainers: [maintainers.jbencina],
   url: 'https://socoemergency.org/emergency/novel-coronavirus/novel-coronavirus-in-sonoma-county/',
   async scraper() {
     const $ = await fetch.page(this.url);
