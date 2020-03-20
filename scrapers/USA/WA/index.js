8cb1a699c0386ee15d9f084e106199cc9bb39885
Larry Davis <lazdnet@gmail.com>
2020-03-19 21:56:05 -0700

    fix: WA, USA

--- scrapers/USA/WA/index.js
+++ scrapers/USA/WA/index.js
@@ -12,31 +12,60 @@ const scraper = {
   type: 'table',
   headless: true,
   aggregate: 'county',
-  async scraper() {
-    const counties = [];
-    const $ = await fetch.headless(this.url);
-    const $th = $('th:contains("(COVID-19) in Washington")');
-    const $table = $th.closest('table');
-    const $trs = $table.find('tbody > tr');
-    $trs.each((index, tr) => {
-      const $tr = $(tr);
-      const cases = parse.number($tr.find('> *:nth-child(2)').text());
-      const deaths = parse.number($tr.find('> *:last-child').text());
-      let county = transform.addCounty(parse.string($tr.find('> *:first-child').text()));
-      if (county === 'Unassigned County') {
-        county = UNASSIGNED;
-      }
-      if (index < 1 || index > $trs.get().length - 2) {
-        return;
-      }
-      counties.push({
-        county,
-        cases,
-        deaths
+  scraper: {
+    '0': async function() {
+      const counties = [];
+      const $ = await fetch.headless(this.url);
+      const $th = $('th:contains("(COVID-19) in Washington")');
+      const $table = $th.closest('table');
+      const $trs = $table.find('tbody > tr');
+      $trs.each((index, tr) => {
+        const $tr = $(tr);
+        const cases = parse.number($tr.find('> *:nth-child(2)').text());
+        const deaths = parse.number($tr.find('> *:last-child').text());
+        let county = transform.addCounty(parse.string($tr.find('> *:first-child').text()));
+        if (county === 'Unassigned County') {
+          county = UNASSIGNED;
+        }
+        if (index < 1 || index > $trs.get().length - 2) {
+          return;
+        }
+        counties.push({
+          county,
+          cases,
+          deaths
+        });
       });
-    });
-    counties.push(transform.sumData(counties));
-    return counties;
+      counties.push(transform.sumData(counties));
+      return counties;
+    },
+    '2020-3-19': async function() {
+      const counties = [];
+      const $ = await fetch.headless(this.url);
+      const $table = $('caption:contains("Number of Individuals Tested")')
+        .first()
+        .closest('table');
+      const $trs = $table.find('tbody > tr');
+      $trs.each((index, tr) => {
+        const $tr = $(tr);
+        const cases = parse.number($tr.find('td:nth-child(2)').text());
+        const deaths = parse.number($tr.find('td:last-child').text());
+        let county = transform.addCounty(parse.string($tr.find('> *:first-child').text()));
+        if (county === 'Unassigned County') {
+          county = UNASSIGNED;
+        }
+        if (index < 1 || index > $trs.get().length - 2) {
+          return;
+        }
+        counties.push({
+          county,
+          cases,
+          deaths
+        });
+      });
+      counties.push(transform.sumData(counties));
+      return counties;
+    }
   }
 };
 
