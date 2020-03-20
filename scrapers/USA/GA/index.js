463c61492ea415b04a31d15835773e54d41d2d03
Larry Davis <lazdnet@gmail.com>
2020-03-20 16:56:45 -0700

    fix: use new geography lib

--- scrapers/USA/GA/index.js
+++ scrapers/USA/GA/index.js
@@ -1,6 +1,6 @@
 import * as fetch from '../../../lib/fetch.js';
 import * as parse from '../../../lib/parse.js';
-import * as transform from '../../../lib/transform.js';
+import * as geography from '../../../lib/geography.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -17,7 +17,7 @@ const scraper = {
     const $trs = $table.find('tbody > tr');
     $trs.each((index, tr) => {
       const $tr = $(tr);
-      const county = transform.addCounty(parse.string($tr.find('td:first-child').text()));
+      const county = geography.addCounty(parse.string($tr.find('td:first-child').text()));
       const cases = parse.number($tr.find('td:last-child').text());
       counties.push({ county, cases });
     });
