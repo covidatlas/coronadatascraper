93c2a8055686b8e5222bfc8d2a9b815b8cf41e47
Paul Gauthier <github.com@paulg.org>
2020-03-21 11:48:11 -0700

    Filled in 0s for counties not listed in MI table

--- scrapers/USA/MI/index.js
+++ scrapers/USA/MI/index.js
@@ -13,6 +13,92 @@ const scraper = {
   type: 'table',
   aggregate: 'county',
 
+  _counties: [
+    'Alcona County',
+    'Alger County',
+    'Allegan County',
+    'Alpena County',
+    'Antrim County',
+    'Arenac County',
+    'Baraga County',
+    'Barry County',
+    'Bay County',
+    'Benzie County',
+    'Berrien County',
+    'Branch County',
+    'Calhoun County',
+    'Cass County',
+    'Charlevoix County',
+    'Cheboygan County',
+    'Chippewa County',
+    'Clare County',
+    'Clinton County',
+    'Crawford County',
+    'Delta County',
+    'Dickinson County',
+    'Eaton County',
+    'Emmet County',
+    'Genesee County',
+    'Gladwin County',
+    'Gogebic County',
+    'Grand Traverse County',
+    'Gratiot County',
+    'Hillsdale County',
+    'Houghton County',
+    'Huron County',
+    'Ingham County',
+    'Ionia County',
+    'Iosco County',
+    'Iron County',
+    'Isabella County',
+    'Jackson County',
+    'Kalamazoo County',
+    'Kalkaska County',
+    'Kent County',
+    'Keweenaw County',
+    'Lake County',
+    'Lapeer County',
+    'Leelanau County',
+    'Lenawee County',
+    'Livingston County',
+    'Luce County',
+    'Mackinac County',
+    'Macomb County',
+    'Manistee County',
+    'Marquette County',
+    'Mason County',
+    'Mecosta County',
+    'Menominee County',
+    'Midland County',
+    'Missaukee County',
+    'Monroe County',
+    'Montcalm County',
+    'Montmorency County',
+    'Muskegon County',
+    'Newaygo County',
+    'Oakland County',
+    'Oceana County',
+    'Ogemaw County',
+    'Ontonagon County',
+    'Osceola County',
+    'Oscoda County',
+    'Otsego County',
+    'Ottawa County',
+    'Presque Isle County',
+    'Roscommon County',
+    'Saginaw County',
+    'St. Clair County',
+    'St. Joseph County',
+    'Sanilac County',
+    'Schoolcraft County',
+    'Shiawassee County',
+    'Tuscola County',
+    'Van Buren County',
+    'Washtenaw County',
+    'Wayne County',
+    'Wexford County'
+  ],
+
   async scraper() {
     // The webpage breaks out Detroit, which is in Wayne County.
     // The table does not include Detroit's numbers in the Wayne County totals.
@@ -26,7 +112,7 @@ const scraper = {
     const $table = $cap.closest('table');
     const $trs = $table.find('tbody > tr');
 
-    const counties = [];
+    let counties = [];
 
     $trs.each((index, tr) => {
       const $tr = $(tr);
@@ -61,6 +147,8 @@ const scraper = {
         deaths
       });
     });
+
+    counties = geography.addEmptyRegions(counties, this._counties, 'county');
     counties.push(transform.sumData(counties));
 
     return counties;
