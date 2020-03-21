f0a57f6469c7cee044248b00b4d9b20ac64cec38
Paul Boal <paul.boal@amitechsolutions.com>
2020-03-21 00:15:39 -0500

    fixed maintainer information

--- scrapers/USA/OK/index.js
+++ scrapers/USA/OK/index.js
@@ -2,6 +2,7 @@ import * as fetch from '../../../lib/fetch.js';
 import * as parse from '../../../lib/parse.js';
 import * as transform from '../../../lib/transform.js';
 import * as geography from '../../../lib/geography.js';
+import maintainers from '../../../lib/maintainers.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -17,16 +18,7 @@ const scraper = {
   source: {
     name: 'Oklahoma State Deparment of Health'
   },
-  maintainers: [
-    {
-      name: 'Paul Boal',
-      email: 'paul.boal@amitechsolutions.com',
-      url: 'https://amitechsolutions.com',
-      github: 'paulboal',
-      country: 'USA',
-      flag: '🇺🇸'
-    }
-  ],
+  maintainers: [maintainers.paulboal],
   _countyMap: {},
   _counties: [
     'Adair County',
