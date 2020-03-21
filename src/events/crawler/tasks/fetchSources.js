2ff45cff0d915be596e1af649d9694197cd7d871
Ryan Block <rblock@gmail.com>
2020-03-21 09:17:49 -0700

    Fix paths for scraper fetcher

--- src/events/crawler/tasks/fetchSources.js
+++ src/events/crawler/tasks/fetchSources.js
@@ -1,9 +1,12 @@
 import fastGlob from 'fast-glob';
+import { join } from 'path';
 
 export default async args => {
   console.log(`⏳ Fetching scrapers`);
-  const filePaths = await fastGlob(['./scrapers/**/*.js']);
-  const sources = await Promise.all(filePaths.map(filePath => import(`../${filePath}`))).then(modules => [...modules.map(module => module.default)]);
+  const scrapers = join(__dirname, '..', 'scrapers', '**', '*.js');
+  let filePaths = await fastGlob([scrapers]);
+  filePaths = filePaths.filter(file => !file.endsWith('.test.js'));
+  const sources = await Promise.all(filePaths.map(filePath => import(filePath))).then(modules => [...modules.map(module => module.default)]);
   console.log(`✅ Fetched ${sources.length} scrapers!`);
 
   return { ...args, sources };
