bc383deb0fc41ad78130699b06f3bd06d11a4280
Nils Ole Tippenhauer <noleti@gmx.de>
2020-03-21 18:09:38 +0100

    German state parsers with metadata

---
+++ scrapers/DEU/SL/index.js
@@ -0,0 +1,49 @@
+import * as fetch from '../../../lib/fetch.js';
+import * as datetime from '../../../lib/datetime.js';
+
+const scraper = {
+  state: 'SL',
+  country: 'DEU',
+  timeseries: true,
+  url: 'https://covid19-germany.appspot.com/timeseries/DE-SL',
+  type: 'json',
+  curators: [
+    {
+      name: 'Dr. Jan-Philip Gehrcke',
+      email: 'jgehrcke@googlemail.com'
+    }
+  ],
+  sources: [
+    {
+      url: 'https://github.com/jgehrcke/covid-19-germany-gae',
+      name: 'covid-19-germany-gae',
+      description: 'Official numbers published by public health offices (Gesundheitsaemter) in Germany'
+    }
+  ],
+  async scraper() {
+    const dcases = await fetch.json(`${this.url}/cases`);
+    const scrapeDate = process.env.SCRAPE_DATE ? process.env.SCRAPE_DATE : datetime.getYYYYMMDD();
+
+    let latestDate = new Date(Object.keys(dcases.data[dcases.data.length - 1])[0]);
+
+    if (datetime.dateIsBefore(latestDate, scrapeDate)) {
+      console.error('🚨 Timeseries for DEU-%s has not been updated, latest date is using %s instead of %s', this.state, datetime.getYYYYMMDD(latestDate), datetime.getYYYYMMDD(scrapeDate));
+      latestDate = datetime.getYYYYMMDD(latestDate, '-');
+    } else {
+      latestDate = datetime.getYYYYMMDD(scrapeDate, '-');
+    }
+
+    const ddeaths = await fetch.json(`${this.url}/deaths`);
+
+    return {
+      country: this.country,
+      state: this.state,
+      cases: Object.values(dcases.data[dcases.data.length - 1])[0],
+      deaths: Object.values(ddeaths.data[dcases.data.length - 1])[0],
+      coordinates: [6.49, 49.22],
+      population: 0.99 * 10 ** 6
+    };
+  }
+};
+
+export default scraper;
