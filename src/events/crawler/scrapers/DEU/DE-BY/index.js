ae6cc3d2888eac9ea1a86b95719ffad561ff513d
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-03-21 18:51:14 +0100

    Scraper DEU/DE-BY: PR feedback
    - use geography.getName(this)
    - move rowToResult func to parent obj
    - polish error msgs

--- src/events/crawler/scrapers/DEU/DE-BY/index.js
+++ src/events/crawler/scrapers/DEU/DE-BY/index.js
@@ -1,5 +1,6 @@
 import * as fetch from '../../../lib/fetch.js';
 import * as datetime from '../../../lib/datetime.js';
+import * as geography from '../../../lib/geography.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
@@ -23,20 +24,16 @@ const scraper = {
       url: 'https://github.com/jgehrcke/covid-19-germany-gae'
     }
   ],
+  _rowToResult: row => {
+    return {
+      cases: parseInt(row['DE-BY_cases'], 10),
+      deaths: parseInt(row['DE-BY_deaths'], 10),
+      coordinates: [11.497, 48.79],
+      population: 13 * 10 ** 6
+    };
+  },
   async scraper() {
     const data = await fetch.csv(this.url);
-
-    function rowToResult(row) {
-      return {
-        cases: parseInt(row['DE-BY_cases'], 10),
-        deaths: parseInt(row['DE-BY_deaths'], 10),
-        coordinates: [11.497, 48.79],
-        population: 13 * 10 ** 6
-      };
-    }
-
-    // location string for log and error msgs.
-    const loc = `${this.country}, ${this.state}`;
 
     // Rely on dataset to be sorted by time, in direction past -> future.
     const [lastRow] = data.slice(-1);
@@ -44,30 +41,31 @@ const scraper = {
     if (!process.env.SCRAPE_DATE) {
       // We're asked to return the current state. Rely on last entry in time
       // series to be fresh and return that.
-      return rowToResult(lastRow);
+      return this._rowToResult(lastRow);
     }
 
     // Get historical data for a specific day.
     const queryDate = new Date(process.env.SCRAPE_DATE);
     const queryDayString = datetime.getYYYYMD(queryDate);
     const lastDateInTimeseries = new Date(lastRow.time_iso8601);
+    const lastDateInTimeseriesString = datetime.getYYYYMD(lastDateInTimeseries);
     const firstDateInTimeseries = new Date(data.slice(0)[0].time_iso8601);
 
     // Handle the two special cases where queryDate is before or after the time
     // interval covered by the timeseries.
     if (queryDate > lastDateInTimeseries) {
-      console.error(`  🚨 timeseries for ${loc}: SCRAPE_DATE ${queryDayString} is newer than last sample time ${lastDateInTimeseries}. Use last sample anyway`);
-      return rowToResult(lastRow);
+      console.error(`  🚨 timeseries for ${geography.getName(this)}: SCRAPE_DATE ${queryDayString} is newer than last sample time ${lastDateInTimeseriesString}. Use last sample anyway`);
+      return this._rowToResult(lastRow);
     }
 
     if (queryDate < firstDateInTimeseries) {
-      throw new Error(`Timeseries starts later than SCRAPE_DATE ${queryDayString}`);
+      throw new Error(`Timeseries for ${geography.getName(this)} starts later than SCRAPE_DATE ${queryDayString}`);
     }
 
     // Search through time series for the query date.
     for (const row of data) {
       if (datetime.getYYYYMD(new Date(row.time_iso8601)) === queryDayString) {
-        return rowToResult(row);
+        return this._rowToResult(row);
       }
     }
 
