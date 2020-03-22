6add59e67fbc4457b143bc517866b5b00dce4997
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-03-22 21:59:23 +0100

    fix: fetchSources: do not treat _-prefixed files as scrapers

--- src/events/crawler/tasks/fetchSources.js
+++ src/events/crawler/tasks/fetchSources.js
@@ -1,11 +1,16 @@
 import fastGlob from 'fast-glob';
-import { join } from 'path';
+import { join, basename } from 'path';
 
 export default async args => {
   console.log(`⏳ Fetching scrapers`);
   const scrapers = join(__dirname, '..', 'scrapers', '**', '*.js').replace(/\\/g, '/');
   let filePaths = await fastGlob([scrapers]);
   filePaths = filePaths.filter(file => !file.endsWith('.test.js'));
+
+  // Enable shared code files in the scrapers directory w/o treating them
+  // as scrapers. See #196.
+  filePaths = filePaths.filter(file => !basename(file).startsWith('_'));
+
   const sources = await Promise.all(filePaths.map(filePath => import(filePath))).then(modules => [
     ...modules.map(module => module.default)
   ]);
