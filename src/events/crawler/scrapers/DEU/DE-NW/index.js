7067a358015630cddb348fe2f3c173752e055cb4
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-03-22 17:36:50 +0100

    Add DE-NW scraper

---
+++ src/events/crawler/scrapers/DEU/DE-NW/index.js
@@ -0,0 +1,46 @@
+import deuScraperCommon from '../_shared.js';
+
+const scraper = {
+  country: 'DEU',
+  state: 'DE-NW', // ISO 3166 notation
+  url: 'https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/data.csv',
+  type: 'json',
+  timeseries: true,
+  sources: [
+    {
+      name: 'Robert Koch-Institut (RKI), Nordrhein-Westfalen ministry of health',
+      description:
+        'RKI, Nordrhein-Westfalen ministry of health, double-checked with ZEIT ONLINE, curated by JPG, see "curators"',
+      url: 'https://github.com/jgehrcke/covid-19-germany-gae'
+    }
+  ],
+  curators: [
+    {
+      name: 'Dr. Jan-Philip Gehrcke',
+      email: 'jgehrcke@googlemail.com',
+      url: 'https://gehrcke.de',
+      github: 'jgehrcke',
+      twitter: 'gehrcke'
+    }
+  ],
+  maintainers: [
+    {
+      name: 'Dr. Jan-Philip Gehrcke',
+      email: 'jgehrcke@googlemail.com',
+      url: 'https://gehrcke.de',
+      github: 'jgehrcke',
+      twitter: 'gehrcke'
+    }
+  ],
+  _rowToResult: row => {
+    return {
+      cases: parseInt(row['DE-NW_cases'], 10),
+      deaths: parseInt(row['DE-NW_deaths'], 10),
+      coordinates: [7.661, 51.433],
+      population: 17.91 * 10 ** 6
+    };
+  },
+  scraper: deuScraperCommon
+};
+
+export default scraper;
