2948e5bedc8aa69e53ab3bb75757ebf811c3aacc
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-03-22 17:45:27 +0100

    Add DE-SL scraper

---
+++ src/events/crawler/scrapers/DEU/DE-SL/index.js
@@ -0,0 +1,45 @@
+import deuScraperCommon from '../_shared.js';
+
+const scraper = {
+  country: 'DEU',
+  state: 'DE-SL', // ISO 3166 notation
+  url: 'https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/data.csv',
+  type: 'json',
+  timeseries: true,
+  sources: [
+    {
+      name: 'Robert Koch-Institut (RKI), Saarland ministry of health',
+      description: 'RKI, Saarland ministry of health, double-checked with ZEIT ONLINE, curated by JPG, see "curators"',
+      url: 'https://github.com/jgehrcke/covid-19-germany-gae'
+    }
+  ],
+  curators: [
+    {
+      name: 'Dr. Jan-Philip Gehrcke',
+      email: 'jgehrcke@googlemail.com',
+      url: 'https://gehrcke.de',
+      github: 'jgehrcke',
+      twitter: 'gehrcke'
+    }
+  ],
+  maintainers: [
+    {
+      name: 'Dr. Jan-Philip Gehrcke',
+      email: 'jgehrcke@googlemail.com',
+      url: 'https://gehrcke.de',
+      github: 'jgehrcke',
+      twitter: 'gehrcke'
+    }
+  ],
+  _rowToResult: row => {
+    return {
+      cases: parseInt(row['DE-SL_cases'], 10),
+      deaths: parseInt(row['DE-SL_deaths'], 10),
+      coordinates: [7.023, 49.396],
+      population: 0.99 * 10 ** 6
+    };
+  },
+  scraper: deuScraperCommon
+};
+
+export default scraper;
