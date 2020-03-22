f074904edcb89fe648c0d8df3c3a4cfb729aea9e
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-03-22 17:15:42 +0100

    Add DE-HH scraper

---
+++ src/events/crawler/scrapers/DEU/DE-HH/index.js
@@ -0,0 +1,45 @@
+import deuScraperCommon from '../_shared.js';
+
+const scraper = {
+  country: 'DEU',
+  state: 'DE-HH', // ISO 3166 notation
+  url: 'https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/data.csv',
+  type: 'json',
+  timeseries: true,
+  sources: [
+    {
+      name: 'Robert Koch-Institut (RKI), Hamburg ministry of health',
+      description: 'RKI, Hamburg ministry of health, double-checked with ZEIT ONLINE, curated by JPG, see "curators"',
+      url: 'https://github.com/jgehrcke/covid-19-germany-gae'
+    }
+  ],
+  curators: [
+    {
+      name: 'Dr. Jan-Philip Gehrcke',
+      email: 'jgehrcke@googlemail.com',
+      url: 'https://gehrcke.de',
+      github: 'jgehrcke',
+      twitter: 'gehrcke'
+    }
+  ],
+  maintainers: [
+    {
+      name: 'Dr. Jan-Philip Gehrcke',
+      email: 'jgehrcke@googlemail.com',
+      url: 'https://gehrcke.de',
+      github: 'jgehrcke',
+      twitter: 'gehrcke'
+    }
+  ],
+  _rowToResult: row => {
+    return {
+      cases: parseInt(row['DE-HH_cases'], 10),
+      deaths: parseInt(row['DE-HH_deaths'], 10),
+      coordinates: [9.993, 53.551],
+      population: 1.822 * 10 ** 6
+    };
+  },
+  scraper: deuScraperCommon
+};
+
+export default scraper;
