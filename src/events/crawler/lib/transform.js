9c38ab9d963f5f354be8bd713f30432ee7d68db3
Larry Davis <lazdnet@gmail.com>
2020-03-22 00:01:54 -0700

    feat: elevate the priority of summed data sources so they override other data

--- src/events/crawler/lib/transform.js
+++ src/events/crawler/lib/transform.js
@@ -116,6 +116,12 @@ export const sumData = function(dataArray, object) {
       }
     }
   }
+
+  // Automatically elevate the priority
+  if (summedData.priority < 1) {
+    summedData.priority = 1;
+  }
+
   return summedData;
 };
 
