016cbc9419064b14cc500a08241a9d4372710e81
Samir Rayani <samir.rayani@gmail.com>
2020-03-23 19:10:20 -0400

    remove duplicate test files

--- tests/timezone.test.js
+++
@@ -1,8 +0,0 @@
-import { locationData } from './validationHelpers.js';
-
-// simple test for demonstration purposes
-test.each(locationData)('%s: location tz == timeseries tz', (locName, locObj, tsObj) => {
-  if ('tz' in locObj && 'tz' in tsObj) {
-    expect(locObj.tz[0]).toBe(tsObj.tz[0]);
-  }
-});
