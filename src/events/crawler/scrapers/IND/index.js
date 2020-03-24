695c0e9fa843a99fdbc47e148597e6229cdcfd03
Arnav Dashaputra <arugold1@gmail.com>
2020-03-23 23:05:04 -0400

    Removed unneeded comments

--- src/events/crawler/scrapers/IND/index.js
+++ src/events/crawler/scrapers/IND/index.js
@@ -1,20 +1,14 @@
 import * as fetch from '../../lib/fetch.js';
 import * as parse from '../../lib/parse.js';
 import * as rules from '../../lib/rules.js';
-
 import populationState from './populationState.json';
-
-// Set county to this if you only have state data, but this isn't the entire state
-// const UNASSIGNED = '(unassigned)';
 
 const scraper = {
   country: 'IND',
-  url: 'https://www.mohfw.gov.in/', // dashaputra.com/goi
+  url: 'https://www.mohfw.gov.in/',
   type: 'table',
-  // _reject: [{ state: 'Name of State / UT' }],
   aggregate: 'state',
 
-  // Scrape MOHFW.GOV.IN for State, Cases, Deaths, Recovered
   async scraper() {
     this.url = 'https://www.mohfw.gov.in/';
     const $ = await fetch.page(this.url);
