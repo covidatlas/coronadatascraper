7d735f25b61aafe011f0d59c9c53755af5240fbf
Herb Caudill <herb@devresults.com>
2020-03-24 19:10:25 +0100

    events/crawler/scrapers.all.test.js:

--- src/events/crawler/scrapers.all.test.js
+++ src/events/crawler/scrapers.all.test.js
@@ -3,12 +3,16 @@
 
 import { sync as glob } from 'fast-glob';
 import path, { join as _join } from 'path';
-import * as fetch from './lib/fetch.js';
+import { get } from './lib/get.js';
 import { readFile, readJSON } from './lib/fs.js';
 
-jest.mock('./lib/fetch.js');
+jest.mock('./lib/get.js');
 
 const join = (...args) => _join(...args).replace(/\\/g, '/');
+const stripFeatures = d => {
+  delete d.feature;
+  return d;
+};
 
 describe('all scrapers', () => {
   const scrapersDir = join(__dirname, './scrapers/');
@@ -21,8 +25,8 @@ describe('all scrapers', () => {
     const testInputs = glob(join(testDir, '*')).filter(p => !p.includes('expected'));
 
     for (const filePath of testInputs) {
-      const fileName = path.basename(filePath);
-      fetch.mockSources({ [fileName]: readFile(path.resolve(__dirname, filePath)) });
+      const fileName = path.basename(filePath, path.extname(filePath));
+      get.setSources({ [fileName]: readFile(path.resolve(__dirname, filePath)) });
     }
 
     const scraperName = scraperNameFromPath(testDir);
@@ -30,7 +34,8 @@ describe('all scrapers', () => {
     describe(`scraper: ${scraperName}`, () => {
       const s = require(join(testDir, '..', 'index.js')).default;
       it('scrapes latest', async () => {
-        const result = await s.scraper();
+        let result = await s.scraper();
+        result = result.map(stripFeatures);
         const expectedPath = join(testDir, 'expected.json');
         expect(result).toEqual(await readJSON(expectedPath));
       });
