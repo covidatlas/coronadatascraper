dfe9297df9008ec8a47318a96ad3388068c16493
Paul Gauthier <github.com@paulg.org>
2020-03-23 21:06:37 -0700

    Updated Solano County, CA, USA

--- src/events/crawler/scrapers/USA/CA/solano-county.js
+++ src/events/crawler/scrapers/USA/CA/solano-county.js
@@ -11,11 +11,24 @@ const scraper = {
   country: 'USA',
   maintainers: [maintainers.jbencina],
   url: 'http://www.solanocounty.com/depts/ph/coronavirus.asp',
-  async scraper() {
-    const $ = await fetch.page(this.url);
-    const $el = $('*:contains("Number of Positive Cases")').first();
-    const matches = $el.text().match(/Number of Positive Cases in Solano County: (\d+)/);
-    return { cases: parse.number(matches[1]) };
+  scraper: {
+    '0': async function() {
+      const $ = await fetch.page(this.url);
+      const $el = $('*:contains("Number of Positive Cases")').first();
+      const matches = $el.text().match(/Number of Positive Cases in Solano County: (\d+)/);
+      return { cases: parse.number(matches[1]) };
+    },
+    '2020-3-23': async function() {
+      const $ = await fetch.page(this.url);
+
+      const lines = $('font:contains("Confirmed COVID-19")')
+        .html()
+        .split('<br>');
+      const cases = parse.number(lines[1].split(':')[1]);
+      const deaths = parse.number(lines[2].split(':')[1]);
+
+      return { cases, deaths };
+    }
   }
 };
 
