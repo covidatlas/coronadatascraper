a30b60567638b3325d534314c25c26165c352b36
Herb Caudill <herb@devresults.com>
2020-03-24 22:23:28 +0100

    events/crawler/scrapers.all.test.js: scrape by date

--- src/events/crawler/scrapers.all.test.js
+++ src/events/crawler/scrapers.all.test.js
@@ -10,31 +10,32 @@ import { get } from './lib/get.js';
 jest.mock('./lib/get.js');
 
 const join = (...args) => _join(...args).replace(/\\/g, '/');
+
 const stripFeatures = d => {
   delete d.feature;
   return d;
 };
 
+const scrapersDir = join(__dirname, './scrapers/');
+const scraperNameFromPath = s => s.replace(scrapersDir, '').replace('/tests', '');
+
+const testDirs = glob(join(scrapersDir, '**/tests'), { onlyDirectories: true });
+
 describe('all scrapers', () => {
-  const scrapersDir = join(__dirname, './scrapers/');
-
-  const testDirs = glob(join(scrapersDir, '**/tests'), { onlyDirectories: true });
-
-  const scraperNameFromPath = s => s.replace(scrapersDir, '').replace('/tests', '');
-
   for (const testDir of testDirs) {
-    const testInputs = glob(join(testDir, '*')).filter(p => !p.includes('expected'));
     const scraperName = scraperNameFromPath(testDir);
 
-    for (const filePath of testInputs) {
-      const fileName = path.basename(filePath, path.extname(filePath));
-      const source = { [fileName]: readFile(path.resolve(__dirname, filePath)).toString() };
-      get.setSources(source);
-    }
-
     describe(`scraper: ${scraperName}`, () => {
+      const testInputs = glob(join(testDir, '*')).filter(p => !p.includes('expected'));
+
+      for (const filePath of testInputs) {
+        const fileName = path.basename(filePath, path.extname(filePath));
+        const source = { [fileName]: readFile(path.resolve(__dirname, filePath)).toString() };
+        get.setSources(source);
+      }
+
       const s = require(join(testDir, '..', 'index.js')).default;
-      it('scrapes latest', async () => {
+      it('returns latest data', async () => {
         let result = await s.scraper();
         result = result.map(stripFeatures);
         const expectedPath = join(testDir, 'expected.json');
@@ -42,11 +43,20 @@ describe('all scrapers', () => {
         expect(result).toEqual(expected);
       });
 
-      // xxx('scrapes a specific date', async () => {
-      //   process.env.SCRAPE_DATE = '2020-03-16';
-      //   const result = await ESP.scraper();
-      //   expect(result).toEqual(require('./scrapers/ESP/tests/expected.2020-03-16.json'));
-      // });
+      const datedResultsRegex = /expected.(\d{4}-\d{2}-\d{2}).json/i;
+      const datedResults = glob(join(testDir, 'expected.*.json'));
+
+      for (const expectedPath of datedResults) {
+        const date = datedResultsRegex.exec(expectedPath, '$1')[1];
+
+        it('returns data for a specific date', async () => {
+          process.env.SCRAPE_DATE = date;
+          let result = await s.scraper();
+          result = result.map(stripFeatures);
+          const expected = await readJSON(expectedPath);
+          expect(result).toEqual(expected);
+        });
+      }
 
       afterEach(() => {
         delete process.env.SCRAPE_DATE;
