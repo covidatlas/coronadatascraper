1b7449901765424e666d33aa5663c09ecafd8b05
Herb Caudill <herb@devresults.com>
2020-03-24 09:42:00 +0100

    events/crawler/lib/__mocks__/fetch.js: create

---
+++ src/events/crawler/lib/__mocks__/fetch.js
@@ -0,0 +1,17 @@
+﻿let sources;
+const _fetch = jest.requireActual('../fetch.js');
+
+console.log(_fetch);
+
+_fetch.fetch = async url => {
+  const sourceKey = Object.keys(sources).find(key => url.endsWith(key));
+  if (sourceKey === undefined)
+    throw new Error(`You need to provide a mock source for ${url} using fetch.addMockSources().`);
+  return sources[sourceKey];
+};
+
+_fetch.mockSources = s => {
+  sources = s;
+};
+
+module.exports = _fetch;
