297581be61123dd5dd0e28e5b9e72a056eca9ed2
Herb Caudill <herb@devresults.com>
2020-03-25 11:32:22 +0100

    deterministic naming for files

---
+++ src/events/crawler/lib/__test_utils__/sanitizeUrl.js
@@ -0,0 +1,9 @@
+﻿import path from 'path';
+
+export default url => {
+  const ext = path.extname(url);
+  return url
+    .replace(ext, '')
+    .replace(/^https?:\/\//i, '')
+    .replace(/[^a-z0-9]/gi, '_');
+};
--- src/events/crawler/scrapers.all.test.js
+++ src/events/crawler/scrapers.all.test.js
@@ -1,5 +1,6 @@
 ﻿/* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require  */
+/* eslint-disable no-use-before-define */
 
 import { sync as glob } from 'fast-glob';
 import { readFileSync as readFile } from 'fs';
@@ -9,25 +10,30 @@ import { get } from './lib/get.js';
 
 jest.mock('./lib/get.js');
 
-const join = (...args) => _join(...args).replace(/\\/g, '/');
+/**
+This suite automatically tests a scraper's results against its test cases. 
 
-const stripFeatures = d => {
-  delete d.feature;
-  return d;
-};
+To add test cases to a scraper:
+- Add a `tests` folder
+- Add a sample response from the target url. 
+- Add a file named
 
-const scrapersDir = join(__dirname, './scrapers/');
-const scraperNameFromPath = s => s.replace(scrapersDir, '').replace('/tests', '');
 
-const testDirs = glob(join(scrapersDir, '**/tests'), { onlyDirectories: true });
-
+*/
 describe('all scrapers', () => {
-  const testScraper = async (s, expectedPath) => {
-    let result = await s.scraper();
-    result = result.map(stripFeatures);
-    const expected = await readJSON(expectedPath);
-    expect(result).toEqual(expected);
+  const join = (...args) => _join(...args).replace(/\\/g, '/');
+
+  const datedResultsRegex = /expected.(\d{4}-\d{2}-\d{2}).json/i;
+  const getDateFromPath = path => datedResultsRegex.exec(path, '$1')[1];
+  const scraperNameFromPath = s => s.replace(scrapersDir, '').replace('/tests', '');
+
+  const stripFeatures = d => {
+    delete d.feature;
+    return d;
   };
+
+  const scrapersDir = join(__dirname, './scrapers/');
+  const testDirs = glob(join(scrapersDir, '**/tests'), { onlyDirectories: true });
 
   for (const testDir of testDirs) {
     const scraperName = scraperNameFromPath(testDir);
@@ -36,27 +42,32 @@ describe('all scrapers', () => {
       const testInputs = glob(join(testDir, '*')).filter(p => !p.includes('expected'));
 
       for (const filePath of testInputs) {
-        const fileName = path.basename(filePath, path.extname(filePath));
+        const fileName = path.basename(filePath);
         const source = { [fileName]: readFile(path.resolve(__dirname, filePath)).toString() };
         get.setSources(source);
       }
 
-      const scraper = require(join(testDir, '..', 'index.js')).default;
+      const scraperObj = require(join(testDir, '..', 'index.js')).default;
 
       it('returns latest data', async () => {
         const expectedPath = join(testDir, 'expected.json');
-        await testScraper(scraper, expectedPath);
+        let result = await scraperObj.scraper();
+        result = result.map(stripFeatures);
+        const expected = await readJSON(expectedPath);
+        expect(result).toEqual(expected);
       });
 
-      const datedResultsRegex = /expected.(\d{4}-\d{2}-\d{2}).json/i;
       const datedResults = glob(join(testDir, 'expected.*.json'));
 
       for (const expectedPath of datedResults) {
-        const date = datedResultsRegex.exec(expectedPath, '$1')[1];
+        const date = getDateFromPath(expectedPath);
 
         it('returns data for a specific date', async () => {
           process.env.SCRAPE_DATE = date;
-          await testScraper(scraper, expectedPath);
+          let result = await scraperObj.scraper();
+          result = result.map(stripFeatures);
+          const expected = await readJSON(expectedPath);
+          expect(result).toEqual(expected);
         });
       }
 
