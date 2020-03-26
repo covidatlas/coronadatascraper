b11948f21eb1e195cfd1df83f33759e6af59da38
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-03-26 15:21:53 -0700

    apply review comments

--- src/qa/values.quality.qa.js
+++ src/qa/values.quality.qa.js
@@ -7,18 +7,23 @@ export default t => {
       t.test('data is in the correct range', t => {
         for (const key of ['cases', 'deaths', 'recovered', 'tested', 'active']) {
           if (data[key] !== undefined) {
-            t.notOk(Number.isNaN(data[key]));
-            t.ok(Number.isInteger(data[key]));
-            t.ok(data[key] >= 0);
+            t.notOk(Number.isNaN(data[key]), `${key} is NaN`);
+            t.ok(Number.isInteger(data[key]), `${key} is not an integer`);
+            t.ok(data[key] >= 0, `${key} is negative`);
           }
         }
       });
-      t.test('deaths <= cases <= tested', t => {
-        if (data.deaths) {
-          t.ok(data.deaths <= data.cases);
-        }
-        if (data.tested) {
-          t.ok(data.cases <= data.tested);
+      t.test('(deaths | recovered) <= cases <= tested', t => {
+        if (data.cases !== undefined) {
+          if (data.deaths) {
+            t.ok(data.deaths <= data.cases, `death: ${data.deaths} > cases: ${data.cases}`);
+          }
+          if (data.recovered !== undefined) {
+            t.ok(data.recovered <= data.cases, `recovered: ${data.recovered} > cases: ${data.cases}`);
+          }
+          if (data.tested !== undefined) {
+            t.ok(data.cases <= data.tested, `cases: ${data.cases} > tested: ${data.tested}`);
+          }
         }
       });
     });
