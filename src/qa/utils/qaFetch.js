8dfdc3dd26985ba1f43def16da6dc268d59ec862
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-03-26 13:39:04 -0700

    ignore unresolved require

--- src/qa/utils/qaFetch.js
+++ src/qa/utils/qaFetch.js
@@ -1,6 +1,7 @@
 /* eslint-disable global-require */
 let timeseries = {};
 try {
+  // eslint-disable-next-line import/no-unresolved
   timeseries = require('../../../dist/timeseries-byLocation.json');
 } catch (err) {
   console.error('Error loading location and timeseries data');
