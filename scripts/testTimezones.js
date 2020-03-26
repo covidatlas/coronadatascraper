d9ca2d3bf5f54972683589f646edd5fdc84e7576
Herb Caudill <herb@devresults.com>
2020-03-26 19:22:34 +0100

    scripts/testTimezones.js: refactor

--- scripts/testTimezones.js
+++ scripts/testTimezones.js
@@ -1,18 +1,16 @@
 ﻿/* eslint-disable jest/no-commented-out-tests */
 /* eslint-disable no-use-before-define */
 
-const { spawn } = require('child_process');
-const { execSync } = require('child_process');
+const { spawn, execSync } = require('child_process');
 const os = require('os');
 const ianaWin = require('windows-iana');
 
 // This script runs date-related tests in multiple timezones to make sure that we don't have any
-// timezone-related heisenbugs. For example, this will fail on Windows, but only if the system
-// runtime is east of GMT:
+// timezone-related heisenbugs. For example, this will fail if the system runtime is east of GMT:
 //
 // ```js
 // test('timezone heisenbug', () => {
-//   const d = new Date('2020-03-16T00:00:00');
+//   const d = new Date('2020-03-16');
 //   expect(getYYYYMMDD(d)).toBe('2020-03-16'); // sometimes we get 2020-03-15
 // });
 // ```
@@ -26,11 +24,20 @@ const ianaWin = require('windows-iana');
 // ```
 // (replacing `Eastern Standard Time`) with your timezone)
 
-const timezones = ['America/Los_Angeles', 'UTC', 'Europe/Madrid', 'Australia/Sydney'];
+const isWindows = os.platform() === 'win32';
+const currentSystemTimezone = () => Intl.DateTimeFormat().resolvedOptions().timeZone;
+const originalSystemTimezone = currentSystemTimezone();
+
+const timezones = ['America/Chicago', 'UTC', 'Australia/Sydney', originalSystemTimezone];
 
 const run = async () => {
+  console.log(`⏲  Initial timezone: ${originalSystemTimezone}`);
+
   let failures = 0;
   for (const timezone of timezones) {
+    setTZ(timezone);
+    await pause(); // sometimes takes a few ticks to register
+
     console.log('');
     console.log('---------------------------');
     console.log(`⏲  Timezone changed to ${timezone}`);
@@ -42,27 +49,24 @@ const run = async () => {
       console.error(`❌ Tests failed for timezone ${timezone}`);
       failures++;
     }
+    await pause();
   }
-
-  restoreTZ();
 
   console.log('');
   console.log('---------------------------');
   if (failures === 0) console.log(`✅ Tests passed for all ${timezones.length} timezones`);
   else console.error(`❌ Tests failed for ${failures} out of ${timezones.length} timezones`);
 
+  console.log(`⏲  Timezone reset to ${currentSystemTimezone()}`);
+
   return failures ? 1 : 0;
 };
 
-const runTest = async timezone => {
-  setTZ(timezone);
-  await pause(); // sometimes takes a few ticks to register
-  const results = [];
-
-  const pipeOutputToConsole = { stdio: [process.stdin, process.stdout, process.stderr] };
-  const testWorker = spawn('node', ['./scripts/testTimezones_worker.js'], pipeOutputToConsole);
-
+const runTest = async () => {
   return new Promise((resolve, reject) => {
+    const results = [];
+    const pipeOutputToConsole = { stdio: [process.stdin, process.stdout, process.stderr] };
+    const testWorker = spawn('node', ['./scripts/testTimezones_worker.js'], pipeOutputToConsole);
     testWorker.on('close', code => {
       if (code === 0) resolve(results);
       else reject(results);
@@ -71,10 +75,6 @@ const runTest = async timezone => {
 };
 
 // Utilities to change & restore the system timezone
-
-const isWindows = os.platform() === 'win32';
-const currentSystemTimezone = () => Intl.DateTimeFormat().resolvedOptions().timeZone || process.env.TZ;
-const originalSystemTimezone = currentSystemTimezone();
 
 const setTZ = inputTZ => {
   const tz = lookupTZ(inputTZ);
@@ -93,8 +93,9 @@ const setTZ = inputTZ => {
 };
 
 const restoreTZ = () => {
-  if (originalSystemTimezone && originalSystemTimezone !== currentSystemTimezone()) {
-    setTZ(originalSystemTimezone);
+  const tz = originalSystemTimezone;
+  if (tz && tz !== currentSystemTimezone()) {
+    setTZ(tz);
   }
 };
 
@@ -102,17 +103,15 @@ const lookupTZ = inputTZ => {
   if (inputTZ === 'UTC') return inputTZ;
   const tz = isWindows
     ? ianaWin.findWindows(inputTZ) //
-    : ianaWin.findOneIana(inputTZ) || ianaWin.findWindows(inputTZ);
+    : ianaWin.findOneIana(inputTZ) || inputTZ;
   if (!tz) throw new Error(`The timezone '${inputTZ}' does not exist. Please provide a valid Windows or IANA time.`);
   return tz;
 };
 
-const pause = (t = 100) => new Promise(ok => setTimeout(() => ok(), t));
+const pause = (t = 500) => new Promise(ok => setTimeout(() => ok(), t));
 
 // Execution
 
 run().then(result => {
-  restoreTZ();
-  console.log(`System timezone reset to ${currentSystemTimezone()}`);
   process.exit(result);
 });
