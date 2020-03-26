4306232c03bc02ae48820ee90a282532d706c18c
aed3 <32887801+aed3@users.noreply.github.com>
2020-03-25 23:45:37 -0400

    Removed other commit

--- src/events/crawler/scrapers/USA/GA/index.js
+++ src/events/crawler/scrapers/USA/GA/index.js
@@ -180,8 +180,7 @@ const scraper = {
     const $trs = $table.find('tbody > tr');
     $trs.each((index, tr) => {
       const $tr = $(tr);
-      const name = $tr.find('td:first-child').text();
-      let county = geography.addCounty(parse.string(name.replace('Dekalb', 'DeKalb')));
+      let county = geography.addCounty(parse.string($tr.find('td:first-child').text()));
       const cases = parse.number($tr.find('td:last-child').text());
       if (county === 'Unknown County') {
         county = UNASSIGNED;
