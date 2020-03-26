7046886185bf71585ee906cf7c5dd3cbeff22749
Herb Caudill <herb@devresults.com>
2020-03-27 02:42:06 +1100

    events/crawler/lib/isoDate.test.js:

--- src/events/crawler/lib/isoDate.test.js
+++ src/events/crawler/lib/isoDate.test.js
@@ -2,15 +2,20 @@ import { today, now, parse } from './isoDate.js';
 
 const setMockDate = d => jest.spyOn(global.Date, 'now').mockImplementationOnce(() => new Date(d).valueOf());
 
-describe('localDate', () => {
+describe(`isoDate (system timezone: ${Intl.DateTimeFormat().resolvedOptions().timeZone})`, () => {
   describe('today.here', () => {
     it('returns the current date in the current timezone', () => {
-      setMockDate('2020-03-16');
+      setMockDate('2020-03-16T23:45Z');
       expect(today.here()).toEqual('2020-03-16');
     });
   });
 
   describe('today.at', () => {
+    it('returns the current date in UTC', () => {
+      setMockDate('2020-03-16T23:45Z');
+      expect(today.at('UTC')).toEqual('2020-03-16');
+    });
+
     it('returns the current date in Australia', () => {
       setMockDate('2020-03-16T23:45Z');
       expect(today.at('Australia/Sydney')).toEqual('2020-03-17'); // next day
@@ -19,32 +24,41 @@ describe('localDate', () => {
 
   describe('now.here', () => {
     it('returns the current time in the current timezone', () => {
-      setMockDate('2020-03-16T23:45');
+      setMockDate('2020-03-16T23:45Z');
       expect(now.here()).toEqual('2020-03-16T23:45');
     });
   });
 
   describe('now.at', () => {
+    it('returns the current time in UTC', () => {
+      setMockDate('2020-03-16T23:45Z');
+      expect(now.at('UTC')).toEqual('2020-03-16T23:45');
+    });
     it('returns the current time in Australia', () => {
-      setMockDate('2020-03-16T23:45');
-      expect(now.at('Australia/Sydney')).toEqual('2020-03-17T09:45'); // 10 hrs later, next day
+      setMockDate('2020-03-16T23:45Z');
+      expect(now.at('Australia/Sydney')).toEqual('2020-03-17T10:45'); // 11 hrs later, next day
     });
   });
 
   describe('parse', () => {
     it('accepts a JS Date', () => {
       const d = new Date('2020-03-16');
-      expect(parse(d).toString()).toEqual('2020-03-16');
+      expect(parse(d)).toEqual('2020-03-16');
     });
 
     it('accepts an ISO date string', () => {
       const d = '2020-03-16';
-      expect(parse(d).toString()).toEqual('2020-03-16');
+      expect(parse(d)).toEqual('2020-03-16');
     });
 
     it('accepts an ISO datetime string', () => {
       const d = '2020-03-16T23:45:00Z';
-      expect(parse(d).toString()).toEqual('2020-03-16');
+      expect(parse(d)).toEqual('2020-03-16');
+    });
+
+    it('accepts an unpadded ISO date string', () => {
+      const d = '2020-3-1';
+      expect(parse(d)).toEqual('2020-03-01');
     });
   });
 });
