8166b6cfeb2408e03f66917cd55c11790ea93336
Ryan Block <rblock@gmail.com>
2020-03-27 13:16:37 -0700

    Update processor lambda entry

--- src/events/processor/index.js
+++ src/events/processor/index.js
@@ -1,6 +1,9 @@
 const imports = require('esm')(module);
 const arc = require('@architect/functions');
 
+const rateSources = imports('./rate-sources/index.js').default;
+const dedupeLocations = imports('./dedupe-locations/index.js').default;
+const reportScrape = imports('./report/index.js').default;
 const findFeatures = imports('./find-features/index.js').default;
 const findPopulations = imports('./find-populations/index.js').default;
 const cleanLocations = imports('./clean-locations/index.js').default;
@@ -11,8 +14,9 @@ const writeData = imports('./write-data/index.js').default;
  */
 async function crawler(event) {
   const { options } = event;
-
-  const output = await Promise.resolve()
+  const output = await rateSources(event)
+    .then(dedupeLocations)
+    .then(reportScrape)
     .then(options.findFeatures !== false && findFeatures)
     .then(options.findPopulations !== false && findPopulations)
     .then(cleanLocations)
