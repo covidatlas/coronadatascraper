36a7b141756f1aa98a147c6dcfbbee7a12e88139
Larry Davis <lazdnet@gmail.com>
2020-03-27 09:14:14 -0700

    feat: add NYT scraper

---
+++ src/events/crawler/scrapers/USA/NYT.js
@@ -0,0 +1,59 @@
+import * as fetch from '../../lib/fetch.js';
+import * as parse from '../../lib/parse.js';
+import * as geography from '../../lib/geography.js';
+import * as datetime from '../../lib/datetime.js';
+import maintainers from '../../lib/maintainers.js';
+
+const scraper = {
+  maintainers: [maintainers.lazd],
+  url: 'https://github.com/nytimes/covid-19-data',
+  type: 'csv',
+  country: 'USA',
+  timeseries: true,
+  priority: 1,
+  async scraper() {
+    this.url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv';
+    const data = await fetch.csv(this.url, false);
+
+    let scrapeDate = process.env.SCRAPE_DATE ? new Date(`${process.env.SCRAPE_DATE} 12:00:00`) : datetime.getDate();
+    let scrapeDateString = datetime.getYYYYMD(scrapeDate);
+    const lastDateInTimeseries = new Date(`${data[data.length - 1].date} 12:00:00`);
+    const firstDateInTimeseries = new Date(`${data[0].date} 12:00:00`);
+
+    if (scrapeDate > lastDateInTimeseries) {
+      console.error(
+        `  🚨 timeseries for ${geography.getName(
+          this
+        )}: SCRAPE_DATE ${scrapeDateString} is newer than last sample time ${datetime.getYYYYMD(
+          lastDateInTimeseries
+        )}. Using last sample anyway`
+      );
+      scrapeDate = lastDateInTimeseries;
+      scrapeDateString = datetime.getYYYYMD(scrapeDate);
+    }
+
+    if (scrapeDate < firstDateInTimeseries) {
+      throw new Error(`Timeseries starts later than SCRAPE_DATE ${scrapeDateString}`);
+    }
+
+    if (scrapeDate < firstDateInTimeseries) {
+      throw new Error(`Timeseries starts later than SCRAPE_DATE ${scrapeDateString}`);
+    }
+
+    const counties = [];
+    for (const row of data) {
+      if (datetime.getYYYYMD(`${row.date} 12:00:00`) === scrapeDateString) {
+        counties.push({
+          county: geography.getCounty(row.county, row.state),
+          state: geography.getState(row.state),
+          cases: parse.number(row.cases),
+          deaths: parse.number(row.deaths)
+        });
+      }
+    }
+
+    return counties;
+  }
+};
+
+export default scraper;
