dc48c47de55b0a24ab829b663690f009137c1961
Paul Gauthier <github.com@paulg.org>
2020-03-27 08:55:26 -0700

    Refactored getArcGISCSVURL to provide getArcGISCSVURLFromOrgId if you can find the orgId directly

--- src/shared/lib/fetch/index.js
+++ src/shared/lib/fetch/index.js
@@ -197,6 +197,24 @@ export const headless = async (url, date = process.env.SCRAPE_DATE || datetime.g
 };
 
 /**
+ * Open the arcgis iframe and look in the Network/XHR tab for requests with Name of "0".
+ *
+ * Like this one:
+ * https://services7.arcgis.com/4RQmZZ0yaZkGR1zy/arcgis/rest/services/COVID19_testsites_READ_ONLY/FeatureServer/0?f=json
+ *
+ * serverNumber is 7, from services7.arcgis.com
+ * orgId is 4RQmZZ0yaZkGR1zy
+ * layerName is COVID19_testsites_READ_ONLY
+ */
+export const getArcGISCSVURLFromOrgId = async function(serverNumber, orgId, layerName) {
+  const layerMetadata = await json(
+    `https://services${serverNumber}.arcgis.com/${orgId}/arcgis/rest/services/${layerName}/FeatureServer/0?f=json`
+  );
+  const { serviceItemId } = layerMetadata;
+  return `https://opendata.arcgis.com/datasets/${serviceItemId}_0.csv`;
+};
+
+/**
  * Get the URL for the CSV data from an ArcGIS dashboard
  * @param {*} serverNumber the servern number, find this by looking at requests (i.e. https://services1.arcgis.com/ is serverNumber = 1)
  * @param {*} dashboardId the ID of the dashboard, as passed to the iframe that renders it (i.e. https://maps.arcgis.com/apps/opsdashboard/index.html#/ec4bffd48f7e495182226eee7962b422 is dashboardId = ec4bffd48f7e495182226eee7962b422)
@@ -205,9 +223,5 @@ export const headless = async (url, date = process.env.SCRAPE_DATE || datetime.g
 export const getArcGISCSVURL = async function(serverNumber, dashboardId, layerName) {
   const dashboardManifest = await json(`https://maps.arcgis.com/sharing/rest/content/items/${dashboardId}?f=json`);
   const { orgId } = dashboardManifest;
-  const layerMetadata = await json(
-    `https://services${serverNumber}.arcgis.com/${orgId}/arcgis/rest/services/${layerName}/FeatureServer/0?f=json`
-  );
-  const { serviceItemId } = layerMetadata;
-  return `https://opendata.arcgis.com/datasets/${serviceItemId}_0.csv`;
+  return getArcGISCSVURLFromOrgId(serverNumber, orgId, layerName);
 };
