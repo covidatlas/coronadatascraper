33a844db851ef8b60303d49626b2011dec1ce573
chunder <github.com@paulg.org>
2020-03-27 13:53:32 -0700

    fix: HI, USA (#422)
    Co-authored-by: Larry Davis <lazdnet@gmail.com>

--- src/shared/scrapers/USA/HI/index.js
+++ src/shared/scrapers/USA/HI/index.js
@@ -7,7 +7,6 @@ const scraper = {
   country: 'USA',
   state: 'HI',
   priority: 1,
-  url: 'https://health.hawaii.gov/docd/advisories/novel-coronavirus-2019/',
   source: {
     name: 'Hawaii Department of Health - Disease Outbreak Control Division',
     url: 'https://health.hawaii.gov/docd/advisories/novel-coronavirus-2019/'
@@ -23,53 +22,87 @@ const scraper = {
   ],
   aggregate: 'county',
   _counties: ['Hawaii County', 'Honolulu County', 'Kauai County', 'Maui County', 'Kalawao County'],
-  async scraper() {
-    let counties = [];
-    const $ = await fetch.page(this.url);
-    const $table = $('*:contains("Novel Coronavirus in Hawaii")').closest('table');
-    const $trs = $table.find('tr');
+  scraper: {
+    '0': async function() {
+      this.url = 'https://health.hawaii.gov/docd/advisories/novel-coronavirus-2019/';
+      this.type = 'table';
+      let counties = [];
+      const $ = await fetch.page(this.url);
+      const $table = $('*:contains("Novel Coronavirus in Hawaii")').closest('table');
+      const $trs = $table.find('tr');
 
-    let stateDeaths = 0;
-    let rowCellValueList;
+      let stateDeaths = 0;
+      let rowCellValueList;
 
-    // Build county data array.
+      // Build county data array.
 
-    $trs.each((index, tr) => {
-      const $tr = $(tr);
+      $trs.each((index, tr) => {
+        const $tr = $(tr);
 
-      rowCellValueList = $tr.text().split('\n');
+        rowCellValueList = $tr.text().split('\n');
 
-      let county = rowCellValueList[1].trim();
-      const cases = rowCellValueList[2].substr(0, rowCellValueList[2].indexOf(' ')).trim();
+        let county = rowCellValueList[1].trim();
+        const cases = rowCellValueList[2].substr(0, rowCellValueList[2].indexOf(' ')).trim();
 
-      if (county === 'Deaths') {
-        stateDeaths = rowCellValueList[2].substr(0, rowCellValueList[2].indexOf(' ')).trim();
+        if (county === 'Deaths') {
+          stateDeaths = rowCellValueList[2].substr(0, rowCellValueList[2].indexOf(' ')).trim();
 
-        return;
-      }
+          return;
+        }
 
-      county = geography.addCounty(county);
+        county = geography.addCounty(county);
 
-      if (this._counties.includes(geography.addCounty(county))) {
+        if (this._counties.includes(geography.addCounty(county))) {
+          counties.push({
+            county: geography.addCounty(county),
+            cases: parse.number(cases)
+          });
+        }
+      });
+
+      // Post process data array, add empty regions, total deaths and total cases aggregation record.
+
+      counties = geography.addEmptyRegions(counties, this._counties, 'county');
+
+      const aggregationRecord = transform.sumData(counties);
+
+      // Adding extracted death count.
+      aggregationRecord.deaths = parse.number(stateDeaths);
+
+      counties.push(aggregationRecord);
+
+      return counties;
+    },
+    '2020-3-26': async function() {
+      this.url = 'https://health.hawaii.gov/coronavirusdisease2019/';
+      this.type = 'list';
+      let counties = [];
+
+      const $ = await fetch.page(this.url);
+      const $list = $('dd:contains("Honolulu County")')
+        .parent()
+        .find('dd');
+      $list.each((index, row) => {
+        const text = $(row).text();
+        if (text.indexOf('County') === -1) {
+          return;
+        }
+        const pieces = text.split(' ');
+        const county = geography.addCounty(pieces[0].split('’').join(''));
+        const cases = parse.number(pieces[2]);
+
+        // console.log(county, cases);
         counties.push({
-          county: geography.addCounty(county),
-          cases: parse.number(cases)
+          county,
+          cases
         });
-      }
-    });
+      });
 
-    // Post process data array, add empty regions, total deaths and total cases aggregation record.
-
-    counties = geography.addEmptyRegions(counties, this._counties, 'county');
-
-    const aggregationRecord = transform.sumData(counties);
-
-    // Adding extracted death count.
-    aggregationRecord.deaths = parse.number(stateDeaths);
-
-    counties.push(aggregationRecord);
-
-    return counties;
+      counties.push(transform.sumData(counties));
+      counties = geography.addEmptyRegions(counties, this._counties, 'county');
+      // console.log(counties);
+      return counties;
+    }
   }
 };
 
