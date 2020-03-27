7a10503df23874d93bf29fc8650e4876de6505bc
Larry Davis <lazdnet@gmail.com>
2020-03-27 16:09:11 -0700

    fix: parse number words because someone at WA is a terrible person
    * are we serious right now, WA? one, two, and three are terrifically awful ways to report numerical data and I'm very, very angry

--- src/shared/lib/parse.js
+++ src/shared/lib/parse.js
@@ -1,7 +1,25 @@
+const numberMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  ten: 10
+};
+
 /*
   Turn the provided string into a number, ignoring non-numeric data
 */
 export const number = function(string) {
+  if (typeof string === 'string' && numberMap[string.toLowerCase()]) {
+    // yes, I was as angry as you are right now when I wrote this
+    return numberMap[string.toLowerCase()];
+  }
+
   if (typeof string === 'number') return string;
   if (string === '') return 0;
   if (string === undefined) throw new Error('Cannot parse undefined as number.');
