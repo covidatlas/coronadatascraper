08d03d32bed14412f1005357e1ac11b7f12ead84
Herb Caudill <herb@devresults.com>
2020-03-28 13:46:32 +0100

    throw informative error when mocking `get` and mock source not found

--- src/shared/lib/fetch/__mocks__/get.js
+++ src/shared/lib/fetch/__mocks__/get.js
@@ -29,7 +29,8 @@ const mockGet = async url => {
   const urlKey = sanitizeUrl(url);
   const sourceKey = keys.find(key => key.startsWith(urlKey));
   if (sourceKey === undefined) {
-    return undefined;
+    console.log({ url, keys, sourceKey });
+    throw new Error(`You need to provide a mock source for ${url} . The file should be called ${urlKey}`);
   }
   return mockGet.sources[sourceKey];
 };
