afb3637122b88eafc185c4d4300a7ab74ebe9eb6
Herb Caudill <herb@devresults.com>
2020-03-29 13:46:16 +0200

    shared/lib/run-scraper.js: rename location-> source

--- src/shared/lib/run-scraper.js
+++ src/shared/lib/run-scraper.js
@@ -1,41 +1,41 @@
-/* eslint-disable import/prefer-default-export */
-
 import * as datetime from './datetime.js';
 import * as geography from './geography/index.js';
 
 /*
   Run the correct scraper for this location
 */
-export function runScraper(location) {
-  const rejectUnauthorized = location.certValidation === false;
+export default function runScraper(source) {
+  const locationName = geography.getName(source);
+  // console.log(`
+  //   🥄 Scraping ${locationName}
+  //   `);
+
+  const rejectUnauthorized = source.certValidation === false;
   if (rejectUnauthorized) {
     // Important: this prevents SSL from failing
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
   }
-  if (typeof location.scraper === 'function') {
-    return location.scraper();
+  if (typeof source.scraper === 'function') {
+    return source.scraper();
   }
   if (rejectUnauthorized) {
     delete process.env.NODE_TLS_REJECT_UNAUTHORIZED;
   }
-  if (typeof location.scraper === 'object') {
+  if (typeof source.scraper === 'object') {
     // Find the closest date
     const targetDate = process.env.SCRAPE_DATE || datetime.getDate();
     let scraperToUse = null;
-    for (const [date, scraper] of Object.entries(location.scraper)) {
+    for (const [date, scraper] of Object.entries(source.scraper)) {
       if (datetime.dateIsBeforeOrEqualTo(date, targetDate)) {
         scraperToUse = scraper;
       }
     }
     if (scraperToUse === null) {
-      throw new Error(
-        `Could not find scraper for ${geography.getName(location)} at ${
-          process.env.SCRAPE_DATE
-        }, only have: ${Object.keys(location.scraper).join(', ')}`
-      );
+      const keys = Object.keys(source.scraper).join(', ');
+      throw new Error(`Could not find scraper for ${locationName} at ${process.env.SCRAPE_DATE}, only have: ${keys}`);
     }
-    return scraperToUse.call(location);
+    return scraperToUse.call(source);
   }
 
-  throw new Error('Why on earth is the scraper for %s a %s?', geography.getName(location), typeof scraper);
+  throw new Error('Why on earth is the scraper for %s a %s?', locationName, typeof scraper);
 }
