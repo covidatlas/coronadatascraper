54f8fe13f982f823ff3dbf17258e214dfe443304
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-29 01:37:08 +0100

    add it to cli-args

--- src/shared/cli/cli-args.js
+++ src/shared/cli/cli-args.js
@@ -16,6 +16,11 @@ const { argv } = yargs
     description: 'Scrape only the location provided by full name, i.e City, County, State, Country',
     type: 'string'
   })
+  .option('id', {
+    alias: 'i',
+    description: 'Scrape only the location provided by id',
+    type: 'string'
+  })
   .option('skip', {
     alias: 's',
     description: 'Skip the location provided by full name, i.e City, County, State, Country',
