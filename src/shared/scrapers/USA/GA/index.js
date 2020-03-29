0001bf5c725716d6cece7b3cd9bc0a0faf059e6e
aed3 <32887801+aed3@users.noreply.github.com>
2020-03-28 20:54:09 -0400

    Old GA

--- src/shared/scrapers/USA/GA/index.js
+++ src/shared/scrapers/USA/GA/index.js
@@ -1,7 +1,7 @@
 import * as fetch from '../../../lib/fetch/index.js';
 import * as parse from '../../../lib/parse.js';
-import * as transform from '../../../lib/transform.js';
 import * as geography from '../../../lib/geography/index.js';
+import * as transform from '../../../lib/transform.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 const UNASSIGNED = '(unassigned)';
@@ -9,6 +9,12 @@ const UNASSIGNED = '(unassigned)';
 const scraper = {
   state: 'GA',
   country: 'USA',
+  sources: [
+    {
+      url: 'https://dph.georgia.gov',
+      name: 'Georgia Department of Public Health'
+    }
+  ],
   url: 'https://dph.georgia.gov/covid-19-daily-status-report',
   type: 'table',
   aggregate: 'county',
