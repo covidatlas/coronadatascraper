83ba1a95caf323e17c8f6eec35dcfecd6dc79c20
Cameron Chamberlain <git@camjc.com>
2020-03-29 18:14:12 +1100

    harden AUS scrapers

--- src/shared/scrapers/AUS/NSW/index.js
+++ src/shared/scrapers/AUS/NSW/index.js
@@ -1,12 +1,21 @@
+import assert from 'assert';
 import * as parse from '../../../lib/parse.js';
 import * as fetch from '../../../lib/fetch/index.js';
 import maintainers from '../../../lib/maintainers.js';
-import { DeprecatedError } from '../../../lib/errors.js';
 
-/**
- * @param {Cheerio} $row
- */
-const parseRow = $row => parse.number($row.find('td:last-child').text());
+const getKey = rowLabel => {
+  const lowerLabel = rowLabel.toLowerCase();
+  if (lowerLabel.includes('confirmed cases')) {
+    return 'cases';
+  }
+  if (lowerLabel.includes('total')) {
+    return 'tested';
+  }
+  if (lowerLabel.includes('tested and excluded')) {
+    return 'discard';
+  }
+  throw new Error(`unknown row: ${lowerLabel}`);
+};
 
 const scraper = {
   country: 'AUS',
@@ -23,24 +32,22 @@ const scraper = {
   type: 'table',
   url:
     'https://www.health.nsw.gov.au/_layouts/feed.aspx?xsl=1&web=/news&page=4ac47e14-04a9-4016-b501-65a23280e841&wp=baabf81e-a904-44f1-8d59-5f6d56519965&pageurl=/news/Pages/rss-nsw-health.aspx',
-  scraper: {
-    '0': async function() {
-      const $ = await fetch.page(this.url);
-      const anchors = $('channel > item > link');
-      const currentArticleUrl = anchors[0].next.data;
-      const $currentArticlePage = await fetch.page(currentArticleUrl);
-      const $table = $currentArticlePage('.maincontent table:first-of-type');
+  async scraper() {
+    const $ = await fetch.page(this.url);
+    const $anchors = $('channel > item:contains("statistics") > link');
+    const currentArticleUrl = $anchors[0].next.data;
+    const $currentArticlePage = await fetch.page(currentArticleUrl);
 
-      return {
-        state: scraper.state,
-        cases: parseRow($table.find('tbody > tr:nth-child(2)')),
-        tested: parseRow($table.find('tbody > tr:last-child'))
-      };
-    },
-    '2020-3-28': async function() {
-      await fetch.page(this.url);
-      throw new DeprecatedError('NSW page seems to be broken');
-    }
+    const $table = $currentArticlePage('.maincontent table:first-of-type');
+    const $trs = $table.find('tbody > tr:not(:first-child)');
+    const data = { state: this.state };
+    $trs.each((index, tr) => {
+      const $tr = $(tr);
+      const key = getKey($tr.find('td:first-child').text());
+      data[key] = parse.number($tr.find('td:last-child').text());
+    });
+    assert(data.cases > 0, 'Cases is not reasonable');
+    return data;
   }
 };
 
