2914f41cd7a23703a828c91d132aa2244449d17a
Larry Davis <lazdnet@gmail.com>
2020-03-28 22:15:19 -0700

    fix: disable NSW, AUS scraper for now

--- src/shared/scrapers/AUS/NSW/index.js
+++ src/shared/scrapers/AUS/NSW/index.js
@@ -1,6 +1,7 @@
 import * as parse from '../../../lib/parse.js';
 import * as fetch from '../../../lib/fetch/index.js';
 import maintainers from '../../../lib/maintainers.js';
+import { DeprecatedError } from '../../../lib/errors.js';
 
 /**
  * @param {Cheerio} $row
@@ -22,18 +23,24 @@ const scraper = {
   type: 'table',
   url:
     'https://www.health.nsw.gov.au/_layouts/feed.aspx?xsl=1&web=/news&page=4ac47e14-04a9-4016-b501-65a23280e841&wp=baabf81e-a904-44f1-8d59-5f6d56519965&pageurl=/news/Pages/rss-nsw-health.aspx',
-  async scraper() {
-    const $ = await fetch.page(this.url);
-    const anchors = $('channel > item > link');
-    const currentArticleUrl = anchors[0].next.data;
-    const $currentArticlePage = await fetch.page(currentArticleUrl);
-    const $table = $currentArticlePage('.maincontent table:first-of-type');
+  scraper: {
+    '0': async function() {
+      const $ = await fetch.page(this.url);
+      const anchors = $('channel > item > link');
+      const currentArticleUrl = anchors[0].next.data;
+      const $currentArticlePage = await fetch.page(currentArticleUrl);
+      const $table = $currentArticlePage('.maincontent table:first-of-type');
 
-    return {
-      state: scraper.state,
-      cases: parseRow($table.find('tbody > tr:nth-child(2)')),
-      tested: parseRow($table.find('tbody > tr:last-child'))
-    };
+      return {
+        state: scraper.state,
+        cases: parseRow($table.find('tbody > tr:nth-child(2)')),
+        tested: parseRow($table.find('tbody > tr:last-child'))
+      };
+    },
+    '2020-3-28': async function() {
+      await fetch.page(this.url);
+      throw new DeprecatedError('NSW page seems to be broken');
+    }
   }
 };
 
