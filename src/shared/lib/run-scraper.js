2defb913d946c1f23f06eb28a4726a5a7eebb9f9
Herb Caudill <herb@devresults.com>
2020-03-29 09:58:38 +0200

    separate run-scrapers (lives in crawler) and run-scraper (lives in shared)

--- src/shared/lib/run-scraper.js
+++ src/shared/lib/run-scraper.js
@@ -1,49 +1,7 @@
-import path from 'path';
+/* eslint-disable import/prefer-default-export */
+
 import * as datetime from './datetime.js';
 import * as geography from './geography/index.js';
-import reporter from './error-reporter.js';
-
-const numericalValues = ['cases', 'tested', 'recovered', 'deaths', 'active'];
-
-/*
-  Check if the provided data contains any invalid fields
-*/
-function isValid(data) {
-  for (const [prop, value] of Object.entries(data)) {
-    if (value === null) {
-      throw new Error(`Invalid data: ${prop} is null`);
-    }
-    if (Number.isNaN(value)) {
-      throw new Error(`Invalid data: ${prop} is not a number`);
-    }
-  }
-
-  for (const prop of numericalValues) {
-    if (data[prop] !== undefined && typeof data[prop] !== 'number') {
-      throw new Error(`Invalid data: ${prop} is not a number`);
-    }
-  }
-
-  return true;
-}
-
-/*
-  Add output data to the cases array. Input can be either an object or an array
-*/
-function addData(cases, location, result) {
-  if (Array.isArray(result)) {
-    if (result.length === 0) {
-      throw new Error(`Invalid data: scraper for ${geography.getName(location)} returned 0 rows`);
-    }
-    for (const data of result) {
-      if (isValid(data, location)) {
-        cases.push({ ...location, ...data });
-      }
-    }
-  } else if (isValid(result, location)) {
-    cases.push({ ...location, ...result });
-  }
-}
 
 /*
   Run the correct scraper for this location
@@ -81,42 +39,3 @@ export function runScraper(location) {
 
   throw new Error('Why on earth is the scraper for %s a %s?', geography.getName(location), typeof scraper);
 }
-
-const runScrapers = async args => {
-  const { sources, options } = args;
-
-  const locations = [];
-  const errors = [];
-  for (const location of sources) {
-    if (options.skip && geography.getName(location) === options.skip) {
-      continue;
-    }
-    if (
-      options.location &&
-      path.basename(location._path, '.js') !== options.location &&
-      geography.getName(location) !== options.location
-    ) {
-      continue;
-    }
-    if (location.scraper) {
-      try {
-        addData(locations, location, await runScraper(location));
-      } catch (err) {
-        console.error('  ❌ Error processing %s: ', geography.getName(location), err);
-
-        errors.push({
-          name: geography.getName(location),
-          url: location.url,
-          type: err.name,
-          err: err.toString()
-        });
-
-        reporter.logError('scraper failure', 'scraper failed', err.toString(), 'critical', location);
-      }
-    }
-  }
-
-  return { ...args, locations, scraperErrors: errors };
-};
-
-export default runScrapers;
