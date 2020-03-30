2db776f47011a9c3136fb366e919a6d3a27a6d2a
Jacob McGowan <jacobmcgowan78@gmail.com>
2020-03-30 19:03:50 -0400

    Added error checking.

--- src/shared/scrapers/VIR/index.js
+++ src/shared/scrapers/VIR/index.js
@@ -7,7 +7,7 @@ const scraper = {
   url: 'https://doh.vi.gov/covid19usvi',
   sources: [
     {
-      url: 'https://doh.vi.gov',
+      url: 'https://doh.vi.gov/covid19usvi',
       name: 'United States Virgin Islands Department of Health'
     }
   ],
@@ -23,38 +23,47 @@ const scraper = {
       tested: 0,
       deaths: 0
     };
-    const $ = await fetch.page(this.url);
-    const $widget = $('div.widget:has(div.title-box:has(h1:contains(COVID-19 Cases)))');
-    const $paragraphs = $widget.find('div.block-content > p');
 
-    $paragraphs.each((index, paragraph) => {
-      const text = $(paragraph)
-        .text()
-        .toLowerCase();
-      const separatorIndex = text.indexOf(':');
-      const heading = text.substring(0, separatorIndex);
-      const value = text.substring(separatorIndex + 1);
+    try {
+      const $ = await fetch.page(this.url);
+      const $widget = $('div.widget:has(div.title-box:has(h1:contains(COVID-19 Cases)))');
+      const $paragraphs = $widget.find('div.block-content > p');
 
-      switch (heading) {
-        case 'positive':
-          results.cases = parse.number(value);
-          results.tested += results.cases;
-          break;
-        case 'negative':
-          results.tested += parse.number(value);
-          break;
-        // There have been no deaths at the time of writing so different word
-        // choices are being used to better prepare to scrape these numbers.
-        case 'deaths':
-          results.deaths = parse.number(value);
-          break;
-        case 'dead':
-          results.deaths = parse.number(value);
-          break;
-        default:
-          break;
-      }
-    });
+      $paragraphs.each((index, paragraph) => {
+        const text = $(paragraph)
+          .text()
+          .toLowerCase();
+        const separatorIndex = text.indexOf(':');
+        const heading = text.substring(0, separatorIndex);
+        const value = text.substring(separatorIndex + 1);
+
+        switch (heading) {
+          case 'positive':
+            results.cases = parse.number(value);
+            results.tested += results.cases;
+            break;
+          case 'negative':
+            results.tested += parse.number(value);
+            break;
+          // There have been no deaths at the time of writing so different word
+          // choices are being used to better prepare to scrape these numbers.
+          case 'deaths':
+            results.deaths = parse.number(value);
+            break;
+          case 'dead':
+            results.deaths = parse.number(value);
+            break;
+          default:
+            break;
+        }
+      });
+    } catch (error) {
+      throw new Error('Error parsing COVID-19 cases.');
+    }
+
+    if (results.tested === 0 || results.cases === 0) {
+      throw new Error('Unexpected results; there was likely an error parsing COVID-19 cases');
+    }
 
     return results;
   }
