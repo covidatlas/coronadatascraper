96f1899303d7b44b0743eef8ac8f81d19f3d65b1
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-31 23:25:40 +0200

    update version, using assert everywhere, no console.error

--- src/shared/lib/country-levels.js
+++ src/shared/lib/country-levels.js
@@ -1,10 +1,11 @@
+import assert from 'assert';
 import path from 'path';
 import { folderFromZipURL } from './fetch/utils.js';
 import { readJSON } from './fs.js';
 import * as geography from './geography/index.js';
 
+const TARGET_VERSION = 'v1.1.0';
 const LEVELS = ['iso1', 'iso2', 'fips'];
-const TARGET_VERSION = 'v1.0.1';
 
 const levelCache = {};
 const countryLevelsDir = path.join('src', 'shared', 'vendor', 'country-levels');
@@ -21,19 +22,13 @@ export function getIdFromLocation(location) {
 }
 
 export function splitId(id) {
-  if (!isId(id)) {
-    console.error(`  ❌ Wrong id: ${id}`);
-    return {};
-  }
-
+  assert(isId(id), `Wrong id: ${id}`);
   const [level, code] = id.split(':');
   return { level, code };
 }
 
 const getLevelData = async level => {
-  if (!LEVELS.includes(level)) {
-    throw new Error(`Country Level ID: not supported ${level}`);
-  }
+  assert(LEVELS.includes(level), `Country Level ID: not supported ${level}`);
 
   if (levelCache[level]) {
     return levelCache[level];
@@ -55,35 +50,24 @@ export const getLocationData = async id => {
   const levelData = await getLevelData(level);
   const locationData = levelData[code];
 
-  if (!locationData) {
-    throw new Error(`  ❌ Country Level data missing for: ${id}`);
-  }
+  assert(locationData, `Country Level data missing for: ${id}`);
 
   return locationData;
 };
 
 export const getFeature = async id => {
-  let locationData;
-  try {
-    locationData = await getLocationData(id);
-  } catch (err) {
-    console.error(err);
-    return;
-  }
-
+  const locationData = await getLocationData(id);
   const geojsonPath = path.join(countryLevelsDir, 'geojson', locationData.geojson_path);
   const feature = await readJSON(geojsonPath);
   return feature;
 };
 
 export const getPopulation = async id => {
-  let locationData;
-  try {
-    locationData = await getLocationData(id);
-  } catch (err) {
-    console.error(err);
-    return;
-  }
-
+  const locationData = await getLocationData(id);
   return locationData.population;
+};
+
+export const getName = async id => {
+  const locationData = await getLocationData(id);
+  return locationData.name_long || locationData.name;
 };
