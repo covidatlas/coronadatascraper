530b6fc63c35ca87e2c98dc44f30b6595194f733
Ryan Block <rblock@gmail.com>
2020-03-31 14:06:42 -0700

    Fix subtle bug in scrapeDateIs*

--- src/shared/lib/datetime/iso/scrape-date.js
+++ src/shared/lib/datetime/iso/scrape-date.js
@@ -1,4 +1,5 @@
 ﻿import { parse } from './parse.js';
+import { getYYYYMMDD } from './format.js';
 
 /** @returns {string} The value of the SCRAPE_DATE environment variable, as an ISO date */
 export const scrapeDate = () => (process.env.SCRAPE_DATE ? parse(process.env.SCRAPE_DATE) : undefined);
@@ -7,16 +8,16 @@ export const scrapeDate = () => (process.env.SCRAPE_DATE ? parse(process.env.SCR
  * @param {string|Date} d The date to compare to the scrape date.
  * @returns {boolean} true if the date is earlier than the scrape date.
  */
-export const scrapeDateIsBefore = d => scrapeDate() < parse(d);
+export const scrapeDateIsBefore = d => (scrapeDate() ? scrapeDate() : getYYYYMMDD()) < parse(d);
 
 /**
  * @param {string|Date} d The date to compare to the scrape date.
  * @returns {boolean} true if the date is later than the scrape date.
  */
-export const scrapeDateIsAfter = d => scrapeDate() > parse(d);
+export const scrapeDateIsAfter = d => (scrapeDate() ? scrapeDate() : getYYYYMMDD()) > parse(d);
 
 /**
  * @param {string|Date} d The date to compare to the scrape date.
  * @returns {boolean} true if the date is equal to the scrape date.
  */
-export const scrapeDateIs = d => parse(d) === scrapeDate();
+export const scrapeDateIs = d => (scrapeDate() ? scrapeDate() : getYYYYMMDD()) === parse(d);
