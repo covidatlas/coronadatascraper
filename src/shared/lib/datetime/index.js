5d25dabc23c0b3f3e30a2e318f4d103f9a3e90a1
Ryan Block <rblock@gmail.com>
2020-03-31 15:26:56 -0700

    Change default datetime codepath

--- src/shared/lib/datetime/index.js
+++ src/shared/lib/datetime/index.js
@@ -1,4 +1,4 @@
 import iso from './iso/index.js';
 import old from './old/index.js';
 
-export default process.env.USE_ISO_DATETIME ? iso : old;
+export default process.env.USE_OLD_DATETIME ? old : iso;
