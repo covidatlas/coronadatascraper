7618c09080d0e7c1dceff4d5b551ab427519ff08
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-01 02:31:37 +0200

    fix getSmallestLocationStr
    Trying assert but not enabling it yet, as it breaks JHU and AUS/_shared

--- src/shared/lib/geography/index.js
+++ src/shared/lib/geography/index.js
@@ -106,8 +106,11 @@ export const isCity = function(location) {
 
 export const getSmallestLocationStr = function(location) {
   const smallestStr = location.city || location.county || location.state || location.country;
-  assert(smallestStr, `Illegal location: ${location}`);
-  return smallestStr;
+  if (!smallestStr) {
+    // console.log(location);
+  }
+  // assert(smallestStr, `Illegal location: ${JSON.stringify(location)}`);
+  return smallestStr || null;
 };
 
 export const getLocationGranularityName = function(location) {
