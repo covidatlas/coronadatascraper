covidatlas/coronadatascraper

commit 80d60c3c2700193a0617314874b0e75d315a2b30

fix: Louisiana, USA counties -> parishes (#551)
Co-authored-by: Gabriel David Pragin <gabriel@Gabriels-Air.attlocal.net>
Co-authored-by: Gabriel David Pragin <gabriel@vidonnus.com>

src/events/processor/find-features/index.js
@@ -227,7 +227,7 @@ const generateFeatures = ({ locations, report, options, sourceRatings }) => {
               if (!location.county) {
                 continue;
               }
-              if (feature.properties.name === `${location.county.replace('Parish', 'County')}, ${location.state}`) {
+              if (feature.properties.name === `${location.county.replace('County', 'Parish')}, ${location.state}`) {
                 found = true;
                 storeFeature(feature, location);
                 continue locationLoop;

src/shared/scrapers/USA/LA/index.js
@@ -16,7 +16,6 @@ const scraper = {
       name: 'Louisiana Department of Health'
     }
   ],
-  _countyMap: { 'La Salle Parish': 'LaSalle Parish' },
   scraper: {
     '0': async function() {
       this.url = 'http://ldh.la.gov/Coronavirus/';
@@ -37,7 +36,7 @@ const scraper = {
         }
         const cases = parse.number($tr.find('td:last-child').text());
         counties.push({
-          county: geography.getCounty(this._countyMap[county] || county),
+          county: geography.getCounty(county),
           cases
         });
       });
@@ -67,7 +66,7 @@ const scraper = {
         }
         const countyName = `${parse.string(county.PARISH)} Parish`;
         counties.push({
-          county: geography.getCounty(this._countyMap[countyName] || countyName),
+          county: geography.getCounty(countyName),
           cases: parse.number(county.Cases),
           deaths: parse.number(county.Deaths)
         });
@@ -99,7 +98,7 @@ const scraper = {
         }
         const countyName = `${parse.string(county.PARISH)} Parish`;
         counties.push({
-          county: geography.getCounty(this._countyMap[countyName] || countyName),
+          county: geography.getCounty(countyName),
           cases: parse.number(county.Cases),
           deaths: parse.number(county.Deaths)
         });
@@ -134,7 +133,7 @@ const scraper = {
         }
         const countyName = `${parse.string(county.Parish)} Parish`;
         counties.push({
-          county: geography.getCounty(this._countyMap[countyName] || countyName, 'LA'),
+          county: geography.getCounty(countyName, 'LA'),
           cases: parse.number(county.Cases),
           deaths: parse.number(county.Deaths)
         });

commit 393bdcf33fbdff14dc2d74c31555943a9dd88831

fix: unbreak counties, broken by #551

src/events/processor/find-features/index.js
@@ -227,7 +227,10 @@ const generateFeatures = ({ locations, report, options, sourceRatings }) => {
               if (!location.county) {
                 continue;
               }
-              if (feature.properties.name === `${location.county.replace('County', 'Parish')}, ${location.state}`) {
+              if (
+                feature.properties.name === `${location.county.replace('County', 'Parish')}, ${location.state}` ||
+                feature.properties.name === `${location.county.replace('Parish', 'County')}, ${location.state}`
+              ) {
                 found = true;
                 storeFeature(feature, location);
                 continue locationLoop;