2a7aa198245cc7da4a64ce62a0ac7646d0c92e9c
cmcjacob <39151930+cmcjacob@users.noreply.github.com>
2020-03-31 21:22:31 -0600

    lint

--- src/shared/scrapers/USA/CT/index.js
+++ src/shared/scrapers/USA/CT/index.js
@@ -91,7 +91,7 @@ const scraper = {
 
       this.url = `https://portal.ct.gov/-/media/Coronavirus/CTDPHCOVID19summary${new Date(date).getMonth() +
         1}${new Date(date).getUTCDate()}2020.pdf`;
-      
+
       let body;
       try {
         body = await fetch.pdf(this.url);
