f4c1d0a26dc8a4ae33bd15ef1ed358e20ef5b181
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-02 04:42:19 +0200

    ITA migrated to country levels

--- src/shared/scrapers/ITA/index.js
+++ src/shared/scrapers/ITA/index.js
@@ -1,3 +1,4 @@
+import assert from 'assert';
 import * as fetch from '../../lib/fetch/index.js';
 import * as parse from '../../lib/parse.js';
 import datetime from '../../lib/datetime/index.js';
@@ -5,6 +6,30 @@ import * as transform from '../../lib/transform.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
+
+const countryLevelMap = {
+  Abruzzo: 'IT-65',
+  Basilicata: 'IT-77',
+  'P.A. Bolzano': 'IT-BZ',
+  Calabria: 'IT-78',
+  Campania: 'IT-72',
+  'Emilia Romagna': 'IT-45',
+  'Friuli Venezia Giulia': 'IT-36',
+  Lazio: 'IT-62',
+  Liguria: 'IT-42',
+  Lombardia: 'IT-25',
+  Marche: 'IT-57',
+  Molise: 'IT-67',
+  Piemonte: 'IT-21',
+  Puglia: 'IT-75',
+  Sardegna: 'IT-88',
+  Sicilia: 'IT-82',
+  Toscana: 'IT-52',
+  'P.A. Trento': 'IT-32',
+  Umbria: 'IT-55',
+  "Valle d'Aosta": 'IT-23',
+  Veneto: 'IT-34'
+};
 
 const scraper = {
   country: 'ITA',
@@ -36,16 +61,22 @@ const scraper = {
         return row.data.substr(0, 10) === latestDate;
       })
       .map(row => {
+        const state = parse.string(row.denominazione_regione);
+        const stateMapped = countryLevelMap[state];
+        assert(stateMapped, `${state} not found in countryLevelMap`);
+
         return {
           recovered: parse.number(row.dimessi_guariti),
           deaths: parse.number(row.deceduti),
           cases: parse.number(row.totale_casi),
           tested: parse.number(row.tamponi),
-          state: parse.string(row.denominazione_regione)
+          state: stateMapped
         };
       });
 
     states.push(transform.sumData(states));
+
+    console.log(states);
 
     return states;
   }
