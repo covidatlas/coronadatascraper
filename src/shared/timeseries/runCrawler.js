7dde3c8845bc4322c0bf76cf6f3c8d0cf53e3ca7
Larry Davis <lazdnet@gmail.com>
2020-04-01 19:42:49 -0700

    fix: just use argv

--- src/shared/timeseries/runCrawler.js
+++ src/shared/timeseries/runCrawler.js
@@ -7,33 +7,12 @@ const childProcess = require('child_process');
  * This will result in a slower execution time due to the child_process overheads,
  * but it will ensure that timeseries never runs out of memory.
  */
-export default options =>
+export default () =>
   new Promise((resolve, reject) => {
-    let args = [
-      '--findFeatures',
-      options.findFeatures,
-      '--findPopulations',
-      options.findPopulations,
-      '--writeData',
-      options.writeData
-    ];
-
-    if (options.d) {
-      args = args.concat(['-d', options.d]);
-    }
-    if (options.e) {
-      args = args.concat(['-e', options.e]);
-    }
-    if (options.q) {
-      args = args.concat(['-q', options.q]);
-    }
-    if (options.c) {
-      args = args.concat(['-c', options.c]);
-    }
     const child = childProcess.fork(
       path.join('src', 'shared', 'timeseries', 'worker.js'),
       // Pass options as arguments
-      args,
+      process.argv.slice(1),
       {
         stdio: ['ignore', 1, 2, 'ipc']
       }
