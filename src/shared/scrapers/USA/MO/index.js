1f489f95e78fc25e8033ea11ec909e2aa555c792
JZ <jzohrab@gmail.com>
2020-04-03 17:09:42 -0600

    fix: MO datetime import (#643)

--- src/shared/scrapers/USA/MO/index.js
+++ src/shared/scrapers/USA/MO/index.js
@@ -2,7 +2,7 @@ import * as fetch from '../../../lib/fetch/index.js';
 import * as parse from '../../../lib/parse.js';
 import * as transform from '../../../lib/transform.js';
 import * as geography from '../../../lib/geography/index.js';
-import * as datetime from '../../../lib/datetime/index.js';
+import datetime from '../../../lib/datetime/index.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
 const UNASSIGNED = '(unassigned)';
