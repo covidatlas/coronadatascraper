f0ff49906105d2bf146fd1be9432792b91ab33b7
Cameron Chamberlain <git@camjc.com>
2020-04-07 08:19:32 +1000

    Scrape NZ country-level (#689)
    * start to scrape nz
    
    * scrape NZ country-level
    
    * patch
    
    * patch
    
    * patch

--- src/shared/scrapers/AU/ACT/index.js
+++ src/shared/scrapers/AU/ACT/index.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import * as fetch from '../../../lib/fetch/index.js';
 import * as parse from '../../../lib/parse.js';
 import getDataWithTestedNegativeApplied from '../_shared/get-data-with-tested-negative-applied.js';
-import getKey from '../_shared/get-key.js';
+import getKey from '../../../utils/get-key.js';
 import maintainers from '../../../lib/maintainers.js';
 
 const labelFragmentsByKey = [
