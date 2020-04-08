f6b5d035c9cdafecf01927b4324864debdcfc09c
ciscorucinski <ciscorucinski@gmail.com>
2020-04-09 05:34:36 +0900

    cleaning up code

--- src/shared/scrapers/UKR/index.js
+++ src/shared/scrapers/UKR/index.js
@@ -1,7 +1,6 @@
 import * as fetch from '../../lib/fetch/index.js';
 import datetime from '../../lib/datetime/index.js';
 import maintainers from '../../lib/maintainers.js';
-import log from '../../lib/log.js';
 import * as geography from '../../lib/geography/index.js';
 import * as parse from '../../lib/parse.js';
 
@@ -62,30 +61,30 @@ const scraper = {
       name: 'National Security and Defense Council of Ukraine\n'
     }
   ],
-  // Ukraine's URL needs to have the date on it. So this will actually be
-  // changed by the code below.
-  url: 'https://api-covid19.rnbo.gov.ua/data?to=', // append this with YYYY-MM-DD
+  url: 'https://api-covid19.rnbo.gov.ua/data?to=', // append YYYY-MM-DD
   type: 'json',
   aggregate: 'state',
-  maintainers: [maintainers.ciscorucinski], // create an entry in maintainers.js
+  maintainers: [maintainers.ciscorucinski],
   scraper: {
     '0': async function() {
       const regions = [];
       let date = process.env.SCRAPE_DATE;
       date = datetime.getYYYYMMDD(date);
       this.url += date;
+
       const data = await fetch.json(this.url, false, { disableSSL: true });
+
       if (data === null) {
         throw new Error(`UKR: failed to fetch data from ${this.url}.`);
       }
+
       for (const region of data.ukraine) {
         const name =
           region.label.en === 'Kyiv'
-            ? geography.addCounty(region.label.en, 'city') // TODO suffix issue
+            ? geography.addCounty(region.label.en, 'city')
             : geography.addCounty(region.label.en, 'region');
+
         regions.push({
-          // county: name,
-          debug: `${region.country} : ${name} - ${region.label.uk}`,
           cases: parse.number(region.confirmed),
           deaths: parse.number(region.deaths),
           recovered: parse.number(region.recovered),
@@ -93,7 +92,6 @@ const scraper = {
           county: iso2ref[name]
         });
       }
-      log(regions);
       return regions;
     }
   }
