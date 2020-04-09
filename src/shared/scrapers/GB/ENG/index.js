3e8cea427e8c2848d4052d830336c783c6d1b87c
Zsolt Ero <zsolt.ero@gmail.com>
2020-04-09 05:19:33 +0200

    GB using GSS codes

--- src/shared/scrapers/GB/ENG/index.js
+++ src/shared/scrapers/GB/ENG/index.js
@@ -1,8 +1,12 @@
 import * as fetch from '../../../lib/fetch/index.js';
 import * as parse from '../../../lib/parse.js';
 
+import gssCodes from './gss-codes.json';
+
 // Set county to this if you only have state data, but this isn't the entire state
 // const UNASSIGNED = '(unassigned)';
+
+// GSS code to ISO code in Wikidata: https://w.wiki/MQ3
 
 const scraper = {
   country: 'iso1:GB',
@@ -10,14 +14,36 @@ const scraper = {
   url: 'https://www.arcgis.com/sharing/rest/content/items/b684319181f94875a6879bbc833ca3a6/data',
   aggregate: 'county',
   type: 'csv',
+  async _gssCodeMap() {
+    const codeMap = {};
+    for (const row of gssCodes) {
+      const { isoCode, gss } = row;
+      codeMap[gss] = isoCode;
+    }
+    // custom code for Bournemouth, Christchurch and Poole
+    codeMap.E06000058 = 'GB-XBCP';
+    return codeMap;
+  },
+
   async scraper() {
     const data = await fetch.csv(this.url);
     const counties = [];
-    for (const utla of data) {
-      const name = parse.string(utla.GSS_NM);
+
+    const codeMap = await this._gssCodeMap();
+
+    for (const row of data) {
+      const gss = row.GSS_CD;
+      const iso = codeMap[gss];
+      if (!iso) {
+        console.error(`GB, ENG: ${row.GSS_CD} not found in GSS codes`);
+        continue;
+      }
+
+      const clId = `iso2:${iso}`;
+
       counties.push({
-        county: name,
-        cases: parse.number(utla.TotalCases)
+        county: clId,
+        cases: parse.number(row.TotalCases)
       });
     }
     return counties;
