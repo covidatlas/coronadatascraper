1a5da6a8e7541a12e28abfe1cea1f50717fa1ed8
aed3 <32887801+aed3@users.noreply.github.com>
2020-04-08 21:56:14 -0400

    Fixed DC to use time series CSV data
    First, goes to a website to retrieve the cookie necessary to be authorized for the CSV exporter form.
    Then, grabs a value from that form which is needed in the exported CSV's USL. It's different for every time there's a different cookie.
    Next, retrieves the CSV and parses it after removing a title line and extra characters.
    Last, finds the correct column for the date we're looking for.
    
    Well, at least I learned a lot from this mess...

--- src/shared/scrapers/US/DC/index.js
+++ src/shared/scrapers/US/DC/index.js
@@ -1,3 +1,7 @@
+import needle from 'needle';
+import csvParse from 'csv-parse';
+import datetime from '../../../lib/datetime/index.js';
+import maintainers from '../../../lib/maintainers.js';
 import * as fetch from '../../../lib/fetch/index.js';
 import * as parse from '../../../lib/parse.js';
 
@@ -8,86 +12,67 @@ const scraper = {
   state: 'DC',
   country: 'iso1:US',
   county: 'District of Columbia',
+  maintainer: [maintainers.aed3],
+  timeseries: true,
   sources: [
     {
       name: 'DC Public Health Laboratory'
     }
   ],
   url: 'https://coronavirus.dc.gov/page/coronavirus-data',
-  type: 'paragraph',
+  type: 'csv',
   scraper: {
     '0': async function() {
-      const $ = await fetch.page(this.url);
+      const httpResponse = await needle(
+        'get',
+        'https://microstrategy.dc.gov/MicroStrategy/servlet/mstrWeb?evt=3010&src=mstrWeb.3010&loginReq=true&Server=-pWHsIgZI0ELP5C86KOL7JxDg9USFIPR4u6L-ardtrb2AQCUO&ServerAlias=ODC3-MSTRPRDPBLC-IS03.DC.GOV&Project=-KsxelRdCMvENhmUiJoweVcMUphk%3D&Port=-5sIT6ZMkteVaNueM'
+      );
 
-      let cases = 0;
-      cases += parse.number(
-        $('p:contains("Number of PHL positives")')
-          .first()
-          .text()
-          .split(': ')[1]
-      );
-      cases += parse.number(
-        $('p:contains("Number of commercial lab positives")')
-          .first()
-          .text()
-          .split(': ')[1]
-      );
-      return {
-        cases,
-        tested: parse.number(
-          $('p:contains("Number of people tested overall")')
-            .first()
-            .text()
-            .split(': ')[1]
-        )
+      const getOptions = {
+        method: 'get',
+        cookies: httpResponse.cookies
       };
-    },
-    '2020-03-17': async function() {
-      const $ = await fetch.page(this.url);
 
-      let cases = 0;
-      cases += parse.number(
-        $('li:contains("Number of PHL positives")')
-          .first()
-          .text()
-          .split(': ')[1]
+      const form = await fetch.page(
+        'https://microstrategy.dc.gov/MicroStrategy/servlet/mstrWeb?evt=3067&src=mstrWeb.3067&reportID=DA2251A711EA6FB482660080EFA55B20&reportViewMode=1',
+        false,
+        getOptions
       );
-      cases += parse.number(
-        $('li:contains("Number of commercial lab positives")')
-          .first()
-          .text()
-          .split(': ')[1]
-      );
-      return {
-        cases,
-        tested: parse.number(
-          $('li:contains("Number of people tested overall")')
-            .first()
-            .text()
-            .split(': ')[1] || ''
-        )
-      };
-    },
-    '2020-04-01': async function() {
-      const $ = await fetch.page(this.url);
 
-      let cases = 0;
-      cases += parse.number(
-        $('li:contains("Number of PHL positives")')
-          .first()
-          .text()
-          .split(': ')[1]
+      const rb = form('form input[name="rb"]').val();
+
+      const data = await fetch.raw(
+        `https://microstrategy.dc.gov/MicroStrategy/export/Health_Statistics.csv?evt=3012&src=mstrWeb.3012&exportSection=1&exportFormatGrids=csvIServer&exportPlaintextDelimiter=1&exportMetricValuesAsText=0&exportHeadersAsText=0&exportFilterDetails=0&exportOverlapGridTitles=3&SaveReportProperties=*-1.*-1.0.0.0&rb=${rb}`,
+        false,
+        getOptions
       );
-      cases += parse.number(
-        $('li:contains("Number of commercial lab positives")')
-          .first()
-          .text()
-          .split(': ')[1]
-      );
-      return {
-        cases,
-        tested: 3759 // hardcoded because they failed to deliver data for today "Number of people tested overall: Data forthcoming"
-      };
+
+      const json = await new Promise((resolve, reject) => {
+        csvParse(data.slice(data.indexOf('"')).replace(/\0|\*/g, ''), (err, output) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(output);
+          }
+        });
+      });
+
+      const scrapeDate = datetime.scrapeDate() || datetime.today.at('America/New_York');
+      const dataData = datetime.getMDYYYY(scrapeDate);
+      let result = {};
+
+      json[0].forEach((valType, i) => {
+        if (valType === 'KPI Value' && json[1][i] === dataData) {
+          result = {
+            tested: parse.number(json[2][i]),
+            cases: parse.number(json[3][i]),
+            recovered: parse.number(json[4][i]),
+            deaths: parse.number(json[5][i])
+          };
+        }
+      });
+
+      return result;
     }
   }
 };
