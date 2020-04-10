e193c96932e39376c9e10687c5edc05b49e0074c
Jeff Zohrab <jzohrab@gmail.com>
2020-04-10 11:59:51 -0600

    Better log check.

--- scripts/cacheMigration/check_log.sh
+++ scripts/cacheMigration/check_log.sh
@@ -7,8 +7,12 @@ echo "Count of 'Error processing':"
 grep "Error processing" log.txt | wc -l
 
 echo
-echo "Count of 'Cache miss':"
-grep "Cache miss for" log.txt | wc -l
+echo "Count of 'Cache miss' for 'filepath: cache':"
+grep "Cache miss for" log.txt | grep "filepath: cache/" | wc -l
+
+echo
+echo "Count of 'Cache miss' NOT for 'filepath: cache':"
+grep "Cache miss for" log.txt | grep -v "filepath: cache/" | wc -l
 
 echo
 echo "Count of filepath errors (need to fix instrumentation)"
