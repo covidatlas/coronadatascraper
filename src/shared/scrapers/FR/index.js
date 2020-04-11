31c4cc5e04ede68cce34df93d7bfa3c5e752fc20
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-04-11 13:46:31 -0700

    fix: FRA scraper (#741)
    * FRA update
    
    * more fixes for France
    
    * addressed code review
    
    * revert country-levels changes
    
    * revert
    
    * revert
    
    * fix: ISO issues
    
    * revert
    
    * update tests
    
    * Add check for ISO match
    
    * update cache
    
    * add small note about no cases data available regionally
    
    * update tests

--- src/shared/scrapers/FR/index.js
+++ src/shared/scrapers/FR/index.js
@@ -1,69 +1,171 @@
+import assert from 'assert';
+
+import iso1Codes from 'country-levels/iso1.json';
+import iso2Codes from 'country-levels/iso2.json';
+
 import * as fetch from '../../lib/fetch/index.js';
 import * as parse from '../../lib/parse.js';
+import * as transform from '../../lib/transform.js';
+import maintainers from '../../lib/maintainers.js';
 import datetime from '../../lib/datetime/index.js';
 
-import { features } from './features.json';
+import departementsToCountry from './departements-to-country.json';
+import departementsToRegion from './departements-to-region.json';
 
 const scraper = {
   country: 'iso1:FR',
-  url: 'https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv',
   timeseries: true,
+  url: 'https://www.data.gouv.fr/fr/organizations/sante-publique-france/datasets-resources.csv',
+  sources: [
+    {
+      description: 'Santé publique France is the French national agency of public health.',
+      name: 'Santé publique France',
+      url: 'https://www.santepubliquefrance.fr/'
+    }
+  ],
+  maintainers: [maintainers.qgolsteyn],
   priority: 1,
-  // aggregate: 'state', // doesn't seem to be aggregating properly
-  _populations: {
-    Mayotte: 270372,
-    Martinique: 376480
-  },
+  /*
+   * NOTE: No cases data is available for France at the regional level. This scraper instead focuses
+   * on capturing hospitalized, deaths, and testing data regionally.
+   */
   async scraper() {
-    const data = await fetch.csv(this.url, false);
+    const date = datetime.getYYYYMMDD(process.env.SCRAPE_DATE);
 
-    const latestDate = data
-      .map(d => d.date)
-      .sort()
-      .pop();
-    const processDate = process.env.SCRAPE_DATE ? datetime.getYYYYMMDD(new Date(process.env.SCRAPE_DATE)) : undefined;
-    const reportDate = processDate || latestDate;
+    // The latest datasets are posted in this CSV, which is updated daily
+    const datasets = await fetch.csv(this.url, false, { delimiter: ';' });
 
-    const states = {};
+    // We grab the latest relevant dataset URLs
+    const hopitalizedDataset = datasets.find(entry => entry.title.match(/donnees-hospitalieres-covid19-.*.csv/));
+    const testedDataset = datasets.find(entry => entry.title.match(/donnees-tests-covid19-labo-quotidien-.*.csv/));
 
-    for (const row of data) {
-      const data = {};
+    let hopitalizedData = await fetch.csv(hopitalizedDataset.url, false, { delimiter: ';' });
 
-      const granularity = row.granularite;
-      const rowDate = row.date;
+    // Hospitalized data is broken down by gender, we are only interested in all genders
+    hopitalizedData = hopitalizedData.filter(item => item.sexe === '0');
 
-      data.url = row.source_url ? row.source_url : this.url;
+    // Sort by date to ensure accurate cummulative count
+    hopitalizedData = hopitalizedData.sort((a, b) => a.jour - b.jour);
 
-      if (row.cas_confirmes !== undefined && row.cas_confirmes !== '') {
-        data.cases = parse.number(row.cas_confirmes);
-      }
+    let testedData = await fetch.csv(testedDataset.url, false, { delimiter: ';' });
 
-      if (row.deces !== undefined && row.deces !== '') {
-        data.deaths = parse.number(row.deces);
-      }
+    // Testing data is broken down by age group, we are only interested in all age groups
+    testedData = testedData.filter(item => item.clage_covid === '0');
 
-      if (row.gueris !== undefined && row.gueris !== '') {
-        data.recovered = parse.number(row.gueris);
-      }
+    // Sort by date to ensure accurate cummulative count
+    testedData = testedData.sort((a, b) => a.jour - b.jour);
 
-      if ((granularity === 'region' || granularity === 'collectivite-outremer') && rowDate === reportDate) {
-        data.state = row.maille_nom;
+    const testedByDepartements = {};
 
-        const regionCode = row.maille_code.slice(4);
-        data.feature = features.find(item => item.properties.code === regionCode);
-        data.population = this._populations[data.state];
-        if (!data.population) {
-          data.population = data.feature ? data.feature.properties.population : undefined;
+    // Capture cumulative testing data, as the testing data is for the day only
+    for (const item of testedData) {
+      if (datetime.dateIsBeforeOrEqualTo(item.jour, date))
+        testedByDepartements[item.dep] = parse.number(item.nb_test) + (testedByDepartements[item.dep] || 0);
+    }
+
+    const hospitalizedByDepartments = {};
+
+    // Discharged and deaths are cummulative, while hospitalized is current
+    // We can calculate new patients with this formula:
+    // new_patients = n_current_patients - n_yesterdays_patients + n__todays_discharged_patient + n_todays_deaths
+    // We then sum the number of new_patients to get a cummulative number
+    for (const item of hopitalizedData) {
+      if (datetime.dateIsBeforeOrEqualTo(item.jour, date)) {
+        const prev = hospitalizedByDepartments[item.dep];
+        if (prev) {
+          // Get the number of new discharged and deaths
+          const deltaDeaths = parse.number(item.dc) - prev.deaths;
+          const deltaDischarged = parse.number(item.rad) - prev.discharged;
+
+          // Calculate new patients for today according to formula above
+          const newHospitalized = parse.number(item.hosp) - prev.todayHospitalized + deltaDeaths + deltaDischarged;
+
+          hospitalizedByDepartments[item.dep] = {
+            // Store today's number to calculate formula above
+            todayHospitalized: parse.number(item.hosp),
+            // Sum number of new hospitalization
+            hospitalized: prev.hospitalized + newHospitalized,
+            deaths: parse.number(item.dc),
+            discharged: parse.number(item.rad)
+          };
+        } else {
+          // First day with info for this departement
+          hospitalizedByDepartments[item.dep] = {
+            todayHospitalized: parse.number(item.hosp),
+            hospitalized: parse.number(item.hosp),
+            deaths: parse.number(item.dc),
+            discharged: parse.number(item.rad)
+          };
         }
-
-        if (data.state !== '') {
-          states[regionCode] = { ...(states[regionCode] || {}), ...data };
-        }
-      } else if (granularity === 'pays' && rowDate === reportDate) {
-        states.FRA = { ...(states.FRA || {}), ...data };
       }
     }
-    return Object.values(states);
+
+    const overseas = [];
+    const regions = {};
+    const departements = [];
+
+    for (const dep of Object.keys(testedByDepartements)) {
+      if (departementsToCountry[dep]) {
+        // Overseas territories have their own country code
+        // We treat them as country to follow standard set by Johns Hopkins dataset
+        overseas.push({
+          country: departementsToCountry[dep],
+          tested: testedByDepartements[dep],
+          ...hospitalizedByDepartments[dep]
+        });
+      } else {
+        // Other departements are in Metropolitan France
+        const item = {
+          country: 'iso1:FX', // ISO1 code for Metropolitan France
+          county: `iso2:FR-${dep}`,
+          state: departementsToRegion[dep],
+          tested: testedByDepartements[dep],
+          ...hospitalizedByDepartments[dep]
+        };
+
+        // Add to a region dictionary to perform aggregation later
+        const regionArr = regions[departementsToRegion[dep]] || [];
+        regionArr.push(item);
+        regions[departementsToRegion[dep]] = regionArr;
+
+        departements.push(item);
+      }
+    }
+
+    assert(departements.length === 96, 'Invalid number of départements');
+    assert(Object.keys(regions).length === 13, 'Invalid number of metropolitan régions');
+    assert(overseas.length === 5, 'Invalid number of overseas territories');
+
+    const data = [];
+
+    data.push(...overseas);
+
+    data.push(...departements);
+
+    // We aggregate by region
+    for (const reg of Object.keys(regions)) {
+      data.push(
+        transform.sumData(regions[reg], {
+          country: 'iso1:FX', // ISO1 code for Metropolitan France
+          state: reg
+        })
+      );
+    }
+
+    // And for all of Metropolitan France
+    data.push(transform.sumData(departements, { country: 'iso1:FX' }));
+
+    // Make sure all ISO codes exist
+    for (const item of data) {
+      assert(iso1Codes[item.country.replace('iso1:', '')], `Missing iso1 code for country ${item.country}`);
+      assert(!item.state || iso2Codes[item.state.replace('iso2:', '')], `Missing iso2 code for région ${item.state}`);
+      assert(
+        !item.county || iso2Codes[item.county.replace('iso2:', '')],
+        `Missing iso2 code for département ${item.county}`
+      );
+    }
+
+    return data;
   }
 };
 
