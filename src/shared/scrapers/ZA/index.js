998a5f50259079ddcd5ab6aecd8e47023357ccef
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-04-11 14:36:54 -0700

    Add tested and deaths data

--- src/shared/scrapers/ZA/index.js
+++ src/shared/scrapers/ZA/index.js
@@ -5,47 +5,88 @@ import * as parse from '../../lib/parse.js';
 import maintainers from '../../lib/maintainers.js';
 import datetime from '../../lib/datetime/index.js';
 
+const addDateSeparators = date => `${date.substring(0, 4)}-${date.substring(4, 6)}-${date.substring(6, 8)}`;
+
 const scraper = {
   country: 'iso1:ZA',
-  url:
-    'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_confirmed.csv',
+  url: 'https://github.com/dsfsi/covid19za',
   timeseries: true,
   priority: 1,
   type: 'csv',
-  sources: [],
+  sources: [
+    {
+      description:
+        'COVID 19 Data for South Africa created, maintained and hosted by Data Science for Social Impact research group, led by Dr. Vukosi Marivate, at the University of Pretoria.',
+      url: 'https://github.com/dsfsi/covid19za',
+      name: 'Coronavirus COVID-19 (2019-nCoV) Data Repository for South Africa'
+    }
+  ],
   maintainers: [maintainers.qgolsteyn],
   _province: ['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW', 'WC'],
   async scraper() {
     const date = datetime.getYYYYMMDD(process.env.SCRAPE_DATE);
 
-    const casesData = await fetch.csv(this.url, false);
-    const todayData = casesData.find(
-      item =>
-        datetime.parse(
-          `${item.YYYYMMDD.substring(0, 4)}-${item.YYYYMMDD.substring(4, 6)}-${item.YYYYMMDD.substring(6, 8)}`
-        ) === datetime.parse(date)
+    const casesData = await fetch.csv(
+      'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_confirmed.csv',
+      false
+    );
+    const deathsData = await fetch.csv(
+      'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_timeline_deaths.csv',
+      false
+    );
+    const testedData = await fetch.csv(
+      'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_timeline_testing.csv',
+      false
     );
 
-    if (todayData) {
-      const data = [];
-      for (const col of Object.keys(todayData)) {
-        if (this._province.findIndex(item => item === col) > 0) {
-          data.push({
+    const todayCasesData = casesData.find(
+      item => datetime.parse(addDateSeparators(item.YYYYMMDD)) === datetime.parse(date)
+    );
+
+    const todayTestedData = testedData.find(
+      item => datetime.parse(addDateSeparators(item.YYYYMMDD)) === datetime.parse(date)
+    );
+
+    const dataByProvince = {};
+    let nationalData = {};
+
+    if (todayCasesData) {
+      for (const col of Object.keys(todayCasesData)) {
+        if (this._province.findIndex(item => item === col) !== -1) {
+          dataByProvince[col] = {
             state: `iso2:ZA-${col}`,
-            cases: parse.number(todayData[col])
-          });
+            cases: parse.number(todayCasesData[col]),
+            deaths: 0
+          };
         } else if (col === 'total') {
-          data.push({
-            cases: parse.number(todayData[col])
-          });
+          nationalData = {
+            cases: parse.number(todayCasesData[col]),
+            deaths: 0
+          };
         }
       }
-
-      assert(data.length === 9, 'Missing province data');
-
-      return data;
     }
-    throw new Error('No data available for today');
+
+    assert(Object.keys(dataByProvince).length === 9, 'Missing province data');
+
+    if (todayTestedData) {
+      nationalData = {
+        tested: parse.number(todayTestedData.cumulative_tests),
+        ...nationalData
+      };
+    }
+
+    for (const item of deathsData) {
+      if (datetime.dateIsBeforeOrEqualTo(addDateSeparators(item.YYYYMMDD), date)) {
+        if (item.province) {
+          dataByProvince[item.province].deaths += 1;
+        }
+
+        nationalData.deaths += 1;
+      }
+    }
+
+    return [nationalData, ...Object.values(dataByProvince)];
   }
 };
 
