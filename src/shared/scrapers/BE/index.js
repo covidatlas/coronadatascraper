66350d341f9d4502d1bdc251ff9efa1535f35910
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-04-10 21:25:57 -0700

    feat: Belgium scraper

---
+++ src/shared/scrapers/BE/index.js
@@ -0,0 +1,136 @@
+import assert from 'assert';
+
+import iso1Codes from 'country-levels/iso1.json';
+import iso2Codes from 'country-levels/iso2.json';
+import * as fetch from '../../lib/fetch/index.js';
+import * as parse from '../../lib/parse.js';
+import * as transform from '../../lib/transform.js';
+import datetime from '../../lib/datetime/index.js';
+import maintainers from '../../lib/maintainers.js';
+
+import mappings from './iso-mapping.json';
+
+const scraper = {
+  country: 'iso1:BE',
+  url: 'https://epistat.wiv-isp.be/',
+  timeseries: true,
+  priority: 1,
+  type: 'csv',
+  sources: [
+    {
+      description:
+        'Sciensano is a public belgian scientific institution focused on public and animal health assignments.',
+      name: 'Santé publique France',
+      url: 'https://www.sciensano.be/en'
+    }
+  ],
+  maintainers: [maintainers.qgolsteyn],
+  async scraper() {
+    const date = datetime.getYYYYMMDD(process.env.SCRAPE_DATE);
+
+    const casesData = await fetch.csv('https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv', false);
+    const deathsData = await fetch.csv('https://epistat.sciensano.be/Data/COVID19BE_MORT.csv', false);
+    const hospitalizedData = await fetch.csv('https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv', false);
+    const testsData = await fetch.csv('https://epistat.sciensano.be/Data/COVID19BE_tests.csv', false);
+
+    const dataByRegion = {};
+    const dataByProvince = {};
+    let nationalData = { tested: 0 };
+
+    for (const item of casesData) {
+      if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+        if (!dataByProvince[item.REGION]) {
+          dataByProvince[item.REGION] = {};
+        }
+        const regionData = dataByProvince[item.REGION];
+
+        if (!regionData[item.PROVINCE]) {
+          regionData[item.PROVINCE] = {};
+        }
+        const provinceData = regionData[item.PROVINCE];
+
+        provinceData.cases = parse.number(item.CASES) + (provinceData.cases || 0);
+      }
+    }
+
+    for (const item of deathsData) {
+      if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+        if (!dataByRegion[item.REGION]) {
+          dataByRegion[item.REGION] = {};
+        }
+        const regionData = dataByRegion[item.REGION];
+
+        regionData.deaths = parse.number(item.DEATHS) + (regionData.deaths || 0);
+      }
+    }
+
+    for (const item of hospitalizedData) {
+      if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+        if (!dataByProvince[item.REGION]) {
+          dataByProvince[item.REGION] = {};
+        }
+        const regionData = dataByProvince[item.REGION];
+
+        if (!regionData[item.PROVINCE]) {
+          regionData[item.REGION] = {};
+        }
+        const provinceData = regionData[item.PROVINCE];
+
+        provinceData.hospitalized = parse.number(item.NEW_IN) + (provinceData.hospitalized || 0);
+        provinceData.discharged = parse.number(item.NEW_OUT) + (provinceData.discharged || 0);
+      }
+    }
+
+    for (const item of testsData) {
+      if (item.DATE === 'NA' || datetime.dateIsBeforeOrEqualTo(item.DATE, date)) {
+        nationalData.tested += parse.number(item.TESTS);
+      }
+    }
+
+    const data = [];
+
+    for (const reg of Object.keys(dataByRegion)) {
+      let regionData = {
+        state: mappings[reg],
+        ...dataByRegion[reg]
+      };
+
+      if (reg === 'Flanders' || reg === 'Wallonia') {
+        const provinceData = [];
+        for (const prov of Object.keys(dataByProvince[reg])) {
+          provinceData.push({
+            state: mappings[reg],
+            county: mappings[prov],
+            ...dataByProvince[reg][prov]
+          });
+        }
+
+        regionData = transform.sumData(provinceData, regionData);
+        data.push(...provinceData);
+      } else if (reg === 'Brussels' || reg === 'NA') {
+        regionData = {
+          ...dataByProvince[reg][reg],
+          ...regionData
+        };
+      }
+
+      nationalData = transform.sumData([regionData], nationalData);
+      data.push(regionData);
+    }
+
+    data.push(nationalData);
+
+    for (const item of data) {
+      assert(iso1Codes[item.country.replace('iso1:', '')], `Missing iso1 code for country ${item.country}`);
+      assert(!item.state || iso2Codes[item.state.replace('iso2:', '')], `Missing iso2 code for région ${item.state}`);
+      assert(
+        !item.county || iso2Codes[item.county.replace('iso2:', '')],
+        `Missing iso2 code for département ${item.county}`
+      );
+    }
+
+    return data;
+  }
+};
+
+export default scraper;
