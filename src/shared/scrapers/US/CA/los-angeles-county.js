e596b98709f1a871a22e59732e9ed3798b974c46
Cameron Chamberlain <git@camjc.com>
2020-04-12 17:50:06 +1000

    fix US CA Los Angeles
    - Interestingly LA is replacing client-side using
      a small JS file that looks for IDs.
    - Grab the JS file, find the JSON, then use
      that directly.

--- src/shared/scrapers/US/CA/los-angeles-county.js
+++ src/shared/scrapers/US/CA/los-angeles-county.js
@@ -2,29 +2,44 @@ import * as fetch from '../../../lib/fetch/index.js';
 import * as parse from '../../../lib/parse.js';
 import maintainers from '../../../lib/maintainers.js';
 
-// Set county to this if you only have state data, but this isn't the entire state
-// const UNASSIGNED = '(unassigned)';
-
 const scraper = {
-  county: 'Los Angeles County',
-  state: 'CA',
   country: 'iso1:US',
-  maintainers: [maintainers.jbencina],
-  url: 'http://www.publichealth.lacounty.gov/media/Coronavirus/',
-  async scraper() {
-    const $ = await fetch.page(this.url);
-    return {
-      cases: parse.number(
-        $('.counter')
-          .first()
-          .text()
-      ),
-      deaths: parse.number(
-        $('.counter')
-          .last()
-          .text()
-      )
-    };
+  county: 'Los Angeles County',
+  maintainers: [maintainers.jbencina, maintainers.camjc],
+  sources: [
+    {
+      description: 'County of Los Angeles Public Health',
+      name: 'County of Los Angeles Public Health',
+      url: 'http://www.publichealth.lacounty.gov'
+    }
+  ],
+  state: 'CA',
+  type: 'table',
+  url: 'http://www.publichealth.lacounty.gov/media/Coronavirus/js/casecounter.js',
+  scraper: {
+    '0': async function() {
+      const $ = await fetch.page('http://www.publichealth.lacounty.gov/media/Coronavirus/');
+      return {
+        cases: parse.number(
+          $('.counter')
+            .first()
+            .text()
+        ),
+        deaths: parse.number(
+          $('.counter')
+            .last()
+            .text()
+        )
+      };
+    },
+    '2020-03-27': async function() {
+      const $ = await fetch.page(this.url);
+      const { content } = JSON.parse($.text().match(/data = (?<json>[\S\s]+?);/).groups.json);
+      return {
+        cases: parse.number(content.count),
+        deaths: parse.number(content.death)
+      };
+    }
   }
 };
 
