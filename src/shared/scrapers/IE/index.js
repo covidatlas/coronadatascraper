1cb5a071a9d4410fa9f39f1b36f8c362a9e08ca2
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-04-12 15:02:02 -0700

    feat: LV, LT, and IE

--- src/shared/scrapers/IE/index.js
+++ src/shared/scrapers/IE/index.js
@@ -1,18 +1,25 @@
 import * as fetch from '../../lib/fetch/index.js';
 import * as parse from '../../lib/parse.js';
 import datetime from '../../lib/datetime/index.js';
-import * as transform from '../../lib/transform.js';
 import maintainers from '../../lib/maintainers.js';
+// import * as transform from '../../lib/transform.js';
 
 import mapping from './mapping.json';
 
 const scraper = {
   country: 'iso1:IE',
-  url: 'https://onemocneni-aktualne.mzcr.cz/',
+  url: 'https://raw.githubusercontent.com/covid19-eu-zh/covid19-eu-data/master/dataset/covid-19-at.csv',
   timeseries: true,
   priority: 1,
   type: 'csv',
   maintainers: [maintainers.qgolsteyn],
+  sources: [
+    {
+      description: 'COVID-19/SARS-COV-2 Cases in EU by Country, State/Province/Local Authorities, and Date',
+      url: 'https://github.com/covid19-eu-zh/covid19-eu-data',
+      name: 'covid19-eu-data'
+    }
+  ],
   async scraper() {
     const date = datetime.getYYYYMMDD(process.env.SCRAPE_DATE);
 
@@ -37,7 +44,8 @@ const scraper = {
       });
     }
 
-    data.push(transform.sumData(data));
+    // Count is slightly off compared to JHU. Trust JHU but keep regional data.
+    // if (data.length > 0) data.push(transform.sumData(data));
 
     return data;
   }
