b2e7ea82bd6e4b7fc32bd29482fcefb685e45c45
Cameron Chamberlain <git@camjc.com>
2020-04-13 14:53:13 +1000

    fix iso1:VI United States Virgin Islands (#760)
    * fix iso1:VI United States Virgin Islands
    
    * find both PUI UPDATE and LAST UPDATED by being slightly more generic

--- src/shared/scrapers/AU/WA/index.js
+++ src/shared/scrapers/AU/WA/index.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import * as parse from '../../../lib/parse.js';
 import * as fetch from '../../../lib/fetch/index.js';
 import maintainers from '../../../lib/maintainers.js';
-import getDataWithTestedNegativeApplied from '../_shared/get-data-with-tested-negative-applied.js';
+import getDataWithTestedNegativeApplied from '../../../utils/get-data-with-tested-negative-applied.js';
 import getKey from '../../../utils/get-key.js';
 
 const labelFragmentsByKey = [
