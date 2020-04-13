746bc800218fe0fcd4834001b4a79ed3a8a1aec6
aed3 <32887801+aed3@users.noreply.github.com>
2020-04-13 00:57:17 -0400

    feat: added counties to NH (#770)

--- src/shared/scrapers/US/NH/index.js
+++ src/shared/scrapers/US/NH/index.js
@@ -1,3 +1,4 @@
+import cheerioTableparser from 'cheerio-tableparser';
 import * as fetch from '../../../lib/fetch/index.js';
 import * as parse from '../../../lib/parse.js';
 import * as transform from '../../../lib/transform.js';
@@ -7,14 +8,15 @@ import maintainers from '../../../lib/maintainers.js';
 import { DeprecatedError } from '../../../lib/errors.js';
 
 // Set county to this if you only have state data, but this isn't the entire state
-// const UNASSIGNED = '(unassigned)';
+const UNASSIGNED = '(unassigned)';
 
 const scraper = {
   state: 'NH',
   country: 'iso1:US',
   sources: [
     {
-      name: 'New Hampshire Department of Health and Human Services'
+      name: 'New Hampshire Department of Health and Human Services',
+      url: 'https://www.nh.gov/covid19/'
     }
   ],
   url: 'https://www.nh.gov/covid19/documents/case-map.pdf',
@@ -78,6 +80,57 @@ const scraper = {
     '2020-3-31': async function() {
       await fetch.pdf(this.url);
       throw new DeprecatedError('New Hampshire stopped reporting county-level data as of 2020/3/31');
+    },
+    '2020-4-12': async function() {
+      this.url = 'https://www.nh.gov/covid19/';
+      this.type = 'table';
+
+      const $ = await fetch.page(this.url);
+      cheerioTableparser($);
+
+      const $countyTable = $('.county-table');
+      const data = $countyTable.parsetable(true, true, true);
+
+      const counties = [];
+      const Hillsborough = {
+        county: 'Hillsborough County',
+        cases: 0
+      };
+
+      data[0].forEach((name, i) => {
+        if (name.includes('Hillsborough')) {
+          Hillsborough.cases += parse.number(data[1][i]);
+        } else if (!name.match(/County$|Total/)) {
+          counties.push({
+            county: name.includes('TBD') ? UNASSIGNED : geography.addCounty(name),
+            cases: parse.number(data[1][i])
+          });
+        }
+      });
+
+      counties.push(Hillsborough);
+
+      const $summaryTable = $('.summary-list');
+      const summary = $summaryTable.parsetable(true, true, true);
+
+      const getTableNumber = text => {
+        return summary[1][summary[0].findIndex(str => str.includes(text))].match(/[\d,]+/)[0];
+      };
+
+      const casesTotal = parse.number(summary[1][summary[0].indexOf('Number of Persons with COVID-191')]);
+      const deathsTotal = parse.number(getTableNumber('Deaths'));
+      const recoveredTotals = parse.number(getTableNumber('Recovered'));
+      const testedTotals = parse.number(getTableNumber('Tested Negative'));
+
+      const totals = {
+        cases: casesTotal,
+        deaths: deathsTotal,
+        recovered: recoveredTotals,
+        tested: testedTotals
+      };
+
+      counties.push(totals);
+      return counties;
     }
   }
 };
