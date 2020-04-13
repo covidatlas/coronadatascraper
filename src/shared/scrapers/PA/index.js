73c79b2e76a10dc58d6d8f2ce19525829cd72048
Emilio Graff <1@emil.io>
2020-04-13 07:47:52 -0700

    Update src/shared/scrapers/PA/index.js
    Typo
    
    Co-Authored-By: Cameron Chamberlain <git@camjc.com>

--- src/shared/scrapers/PA/index.js
+++ src/shared/scrapers/PA/index.js
@@ -95,7 +95,7 @@ function updateData(data, o) {
 function addEmptyStates(data) {
   // Instead of this, we could actually design the system to do this:
   // 1. Obtain a list of administrative levels entities at whatever granularity the disease data exist
-  // 2. Crate a template data result with all zeros
+  // 2. Create a template data result with all zeros
   // 3. Traverse through data and populate.
   for (const province in provinceToIso2) {
     let found = false;
