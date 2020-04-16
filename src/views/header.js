22f0a38b16b421ed733a2241624df9c9eacadfef
Larry Davis <lazdnet@gmail.com>
2020-04-16 09:46:12 -0700

    feat: make header support theme arg, center search and fixup widths

--- src/views/header.js
+++ src/views/header.js
@@ -5,15 +5,26 @@ const constants = require('@architect/views/constants');
 // eslint-disable-next-line
 const search = require('@architect/views/search');
 
-module.exports = function body(selectedPage = 'home', className = '') {
+module.exports = function body(selectedPage = 'home', className = '', theme = 'dark') {
+  theme = selectedPage === 'home' ? 'light' : theme;
+
   return /* html */ `
-<div class="ca-SiteHeader spectrum-Site-header ${className}">
+<div class="spectrum--${theme} ca-SiteHeader spectrum-Site-header ${className}">
   <sp-action-button quiet class="js-toggleMenu">
     <sp-icon slot="icon" size="s" name="ui:TripleGripper"></sp-icon>
   </sp-action-button>
   <a href="/" class="ca-HeaderLogo"><img src="${arc.static(
-    'logo-banner-light.svg'
+    `logo-banner-${theme}.svg`
   )}" class="ca-Logo ca-Logo--XS" alt="${constants.name} logo"></a>
+  ${
+    selectedPage !== 'home'
+      ? `
+    <div class="ca-SiteHeader-search spectrum--light">
+      ${search({ label: '' })}
+    </div>
+  `
+      : ''
+  }
   <sp-tab-list selected="${selectedPage}" id="tabNav" quiet>
     <sp-tab label="Home" value="home"></sp-tab>
     <!--<sp-tab label="Map" value="map"></sp-tab>-->
@@ -22,15 +33,6 @@ module.exports = function body(selectedPage = 'home', className = '') {
     <!--<sp-tab label="Reports" value="crosscheck"></sp-tab>-->
     <sp-tab label="About" value="about"></sp-tab>
   </sp-tab-list>
-  ${
-    selectedPage !== 'home'
-      ? `
-    <div class="ca-SiteHeader-search">
-      ${search({ label: '' })}
-    </div>
-  `
-      : ''
-  }
 </div>
 `;
 };
