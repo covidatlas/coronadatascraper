231725966dd48970a19f143d3f372665cf6d80e6
Jeff Zohrab <jzohrab@gmail.com>
2020-05-05 21:26:15 -0400

    Add command to combine CDS and Li into one report.

---
+++ src/events/processor/read-li-raw-data/index.js
@@ -0,0 +1,43 @@
+import path from 'path';
+import fs from 'fs';
+
+/** Read generated raw files from dist-raw. */
+const rawDirectory = path.join(__dirname, '..', '..', '..', '..', 'dist-raw-li');
+
+/** Get filenames. */
+function rawFilenames(options) {
+  let suffix = '';
+  if (options.outputSuffix !== undefined) {
+    suffix = options.outputSuffix;
+  } else if (process.env.SCRAPE_DATE) {
+    suffix = `-${process.env.SCRAPE_DATE}`;
+  }
+
+  return {
+    // The sources scraped.
+    sourcesPath: path.join(rawDirectory, `raw-li-sources${suffix}.json`),
+
+    // The full raw data of all scraped locations.
+    locationsPath: path.join(rawDirectory, `raw-li-locations${suffix}.json`)
+  };
+}
+
+/** Reconstitute data from old raw JSON files.  Bump the priority of li sources way up! */
+export default function loadRawPriorityAdjusted(options) {
+  function readJson(f) {
+    if (!f) throw new Error('null file path');
+    if (!fs.existsSync(f)) throw new Error(`Missing raw file ${f}`);
+    const rawdata = fs.readFileSync(f);
+    return JSON.parse(rawdata);
+  }
+
+  const raws = rawFilenames(options);
+  const liSources = readJson(raws.sourcesPath);
+  const liLocations = readJson(raws.locationsPath);
+
+  liLocations.forEach(loc => {
+    loc.priority = 100 + (loc.priority || 0);
+  });
+
+  return { liSources, liLocations };
+}
