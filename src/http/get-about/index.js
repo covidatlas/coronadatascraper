82e692c47159526b014b4509c6c8451b52192523
Ryan Block <rblock@gmail.com>
2020-05-09 15:32:27 -0700

    About page copy

--- src/http/get-about/index.js
+++ src/http/get-about/index.js
@@ -19,7 +19,7 @@ exports.handler = async function http() {
     },
     body: template(
       'About',
-      `
+      /* html */ `
 ${header('about')}
 
 <div class="spectrum-Site-content">
@@ -34,33 +34,65 @@ ${header('about')}
       </div>
 
     <section class="ca-Section">
-      <h1 class="spectrum-Heading spectrum-Heading--XL">About the project</h1>
-      <p class="spectrum-Body spectrum-Body--L">${
-        constants.name
-      } provides easy to understand local-level data – including local charts, maps, and insights – related to the COVID-19 pandemic.</p>
+      <h1 class="spectrum-Heading spectrum-Heading--XL">About COVID Atlas</h1>
+      <p class="spectrum-Body spectrum-Body--L">COVID Atlas is an international team of engineers, designers, and technologists aggregating COVID-19 (aka "Coronavirus") data from official government sources and trusted data providers.</p>
+      <p class="spectrum-Body spectrum-Body--L">Every piece of data collected is open, accessible in the public domain, published in multiple formats, and is available tagged with relevant metadata such as GeoJSON, population / census information, and source citations.</p>
       <p class="spectrum-Body spectrum-Body--L">Our data is sourced exclusively from official public health channels, and verified sources.</p>
 
       <h1 class="spectrum-Heading spectrum-Heading--L">Built by a global community</h1>
-      <p class="spectrum-Body spectrum-Body--M">This project is <a class="spectrum-Link" href="https://github.com/covidatlas/coronadatascraper/">community-developed, open source</a>, and publishes all data, research, and related findings in the public domain.</p>
-      <p class="spectrum-Body spectrum-Body--M">We're working around the clock to release ${
-        constants.name
-      }; in the mean time, if you're a developer or researcher, you can <a class="spectrum-Link" href="https://coronadatascraper.com/data.csv"></a>download our raw local-level dataset</a> or <a class="spectrum-Link" href="https://coronadatascraper.com/features.json">view it displayed on a world map</a>.</p>
-      <p class="spectrum-Body spectrum-Body--M">If you'd like to contribute, please <a class="spectrum-Link" href="https://github.com/covidatlas/coronadatascraper/">send us a PR</a> and join us on <a class="spectrum-Link" href="${
+      <p class="spectrum-Body spectrum-Body--M">This project is <a class="spectrum-Link" href="https://github.com/covidatlas/">community-developed, open source</a>, and publishes all data, research, and related findings in the public domain.</p>
+      <p class="spectrum-Body spectrum-Body--M">If you're a developer or researcher, you can <a class="spectrum-Link" href="https://coronadatascraper.com/data.csv"></a>download our raw local-level dataset</a> or <a class="spectrum-Link" href="https://coronadatascraper.com/features.json">view it displayed on a world map</a>.</p>
+      <p class="spectrum-Body spectrum-Body--M">If you'd like to contribute, please <a class="spectrum-Link" href="https://github.com/covidatlas/">send us a PR</a> and join us on <a class="spectrum-Link" href="${
         constants.slackURL
       }">Slack</a>.</p>
 
-      <p class="spectrum-Body spectrum-Body--M">Please check back for updates!</p>
+      <p class="spectrum-Body spectrum-Body--M">Please also find us on the social network of your choosing:</p>
 
       <nav class="ca-SocialLinks spectrum-Body spectrum-Body--M">
         <a class="spectrum-Link" href="https://twitter.com/covidatlas" target="_blank">Twitter</a>
         <a class="spectrum-Link" href="https://facebook.com/covidatlas" target="_blank">Facebook</a>
         <a class="spectrum-Link" href="https://instagram.com/covidatlas" target="_blank">Instagram</a>
       </nav>
-    </section>
 
+      <h1 class="spectrum-Heading spectrum-Heading--XL">COVID Atlas contributors</h1>
+      <p class="spectrum-Body spectrum-Body--M">COVID Atlas is built and maintained by an international collection of concerned citizens. We are deeply appreciative to every single volunteer around the world who keep COVID Atlas running.</p>
+      <p class="spectrum-Body spectrum-Body--M">Below are some of the current project leads:</p>
+      <ul class="spectrum-Body spectrum-Body--M">
+        <li>
+          <a class="spectrum-Link" href="https://twitter.com/andigalpern">Andi Galpern</a> (San Francisco, CA, US) – Design program management, content, and community
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://camjc.com/">Cam Chamberlain</a> (Melbourne, Australia) – Data scraper lead for Asia and Oceania
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://twitter.com/elission">Elissa Lerner</a> (New York, NY, US) – Copywriting, content, and editing
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://www.linkedin.com/in/jzohrab">Jeff Zohrab</a> (Toronto, Ontario, CA) – Back-end development lead, Back-end onboarding lead
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://twitter.com/lazdnet">Larry Davis</a> (San Francisco, CA, US) – Project co-lead, System architecture co-lead, Front-end lead
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://natebaldw.in/">Nate Baldwin</a> (Salt Lake City, Utah, US) – Product design lead (interim), color schemes, style guides, UX, and UI
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://golsteyn.com/">Quentin Golsteyn</a> (Vancouver, BC, CA) – Data quality and verification lead, data scraper lead for Europe and Africa
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://twitter.com/ryan">Ryan Block</a> (San Francisco, CA, US) – Project co-lead and System architecture co-lead
+        </li>
+        <li>
+          <a class="spectrum-Link" href="https://hyperknot.com/">Zsolt Ero</a> (Hungary) – Back-end development lead
+        </li>
+      </ul>
+
+      <h1 class="spectrum-Heading spectrum-Heading--XL">Additional acknowledgments</h1>
+      <p class="spectrum-Body spectrum-Body--M">COVID Atlas would also like to acknowledge the services provided  by our hosting partner <a class="spectrum-Link" href="https://aws.amazon.com/">Amazon AWS</a>.</p>
+      <p class="spectrum-Body spectrum-Body--M">This project also exists in part thanks to the flexibility provided by <a class="spectrum-Link" href="https://adobe.com/">Adobe</a> and <a class="spectrum-Link" href="https://begin.com/">Begin</a> to some of our contributors.</p>
+    </section>
   ${footer()}
 </div>
-
 `
     )
   };
