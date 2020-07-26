c74915e4e31d091217ceaf73e1b01c888de3fb25
Jeff Zohrab <jzohrab@gmail.com>
2020-07-26 12:59:07 -0400

    Change covidatlas/data page to pull files from s3.

--- src/http/get-data/index.js
+++ src/http/get-data/index.js
@@ -10,7 +10,22 @@ const footer = require('@architect/views/footer');
 const sidebar = require('@architect/views/sidebar');
 
 exports.handler = async function http() {
-  const baseURL = 'https://coronadatascraper.com/';
+  function rptLink(report) {
+    const baseURL = 'https://liproduction-reportsbucket-bhk8fnhv1s76.s3-us-west-1.amazonaws.com/v1/latest';
+    const href = `${baseURL}/${report}`;
+    return `<li><a href="${href}" target="_blank">${report}</a></li>`;
+  }
+
+  const latestLinks = ['latest.json', 'latest.csv'].map(rptLink).join('');
+  const timeseriesLinks = [
+    'timeseries-byLocation.json',
+    'timeseries-jhu.csv',
+    'timeseries-tidy-small.csv',
+    'timeseries.csv'
+  ]
+    .map(rptLink)
+    .join('');
+  const locationLinks = ['locations.json', 'locations.csv', 'features.json'].map(rptLink).join('');
 
   return {
     headers: {
@@ -33,51 +48,40 @@ ${header('data' /* 'ca-SiteHeader--dark spectrum--dark' */)}
       <p class="spectrum-Body spectrum-Body--L">COVID Atlas data is de-duplicated, cross-checked against other sources, and annotated with population data and GeoJSON features. Currently, COVID Atlas crawls and aggregates data from over 150 sources, <strong>covering more 500 states and regions, 3000 counties, and municipalities, and 190 countries</strong>.</p>
       <p class="spectrum-Body spectrum-Body--L">The consolidated, multi-format dataset of granular COVID-19 case information is available in the public domain for anyone to view, download, or access via an API.</p>
       <p class="spectrum-Body spectrum-Body--L">We encourage scientists, researchers, developers, journalists, and anyone else to analyze this dataset, use it to create models and projections, create visualizations, or identify errors and missing data.</p>
-    </div>
-
-    <hr>
 
     <div class="ca-Section row">
-      <section class="ca-SubSection col-xs-12 col-sm-6 col-md-4">
-        <h1 class="spectrum-Heading spectrum-Heading--M">Global timeseries</h1>
-        <p class="spectrum-Body spectrum-Body--M">Worldwide COVID-19 data in timeseries format (starting at 2020-01-22)</p>
-        <!-- <sp-button variant="primary" quiet href="#notimplemented">View data</sp-button> -->
-
-        <overlay-trigger id="trigger" placement="bottom" class="ca-DownloadTrigger">
-          <sp-button variant="primary" slot="trigger">Download</sp-button>
-          <sp-popover dialog slot="click-content" tip open class="ca-DownloadPopover" direction="bottom">
-            <sp-menu>
-              <sp-menu-item download target="_blank" href="${baseURL}timeseries.csv">CSV</sp-menu-item>
-              <sp-menu-item download target="_blank" href="${baseURL}timeseries.json">JSON</sp-menu-item>
-              <sp-menu-item download target="_blank" href="${baseURL}timeseries-byLocation.json">JSON (by location)</sp-menu-item>
-            </sp-menu>
-          </sp-popover>
-        </overlay-trigger>
-      </section>
 
       <section class="ca-SubSection col-xs-12 col-sm-6 col-md-4">
-        <h1 class="spectrum-Heading spectrum-Heading--M">Global daily totals</h1>
+        <h1 class="spectrum-Heading spectrum-Heading--M">Latest: Global daily totals</h1>
         <p class="spectrum-Body spectrum-Body--M">A point-in-time snapshot of worldwide COVID-19 data (updated daily)</p>
-        <!-- <sp-button variant="primary" quiet href="#notimplemented">View data</sp-button> -->
-        </sp-dropdown>
-        <overlay-trigger id="trigger" placement="bottom" class="ca-DownloadTrigger">
-          <sp-button variant="primary" slot="trigger">Download</sp-button>
-          <sp-popover dialog slot="click-content" tip open class="ca-DownloadPopover" direction="bottom">
-          <sp-menu>
-            <sp-menu-item download target="_blank" href="${baseURL}data.csv">CSV</sp-menu-item>
-            <sp-menu-item download target="_blank" href="${baseURL}data.json">JSON</sp-menu-item>
-          </sp-menu>
-          </sp-popover>
-        </overlay-trigger>
       </section>
 
       <section class="ca-SubSection col-xs-12 col-sm-6 col-md-4">
-        <h1 class="spectrum-Heading spectrum-Heading--M">GeoJSON features</h1>
+        <h1 class="spectrum-Heading spectrum-Heading--M">Timeseries: Global timeseries</h1>
+        <p class="spectrum-Body spectrum-Body--M">Worldwide COVID-19 data in timeseries format (starting at 2020-01-22)</p>
+      </section>
+
+      <section class="ca-SubSection col-xs-12 col-sm-6 col-md-4">
+        <h1 class="spectrum-Heading spectrum-Heading--M">Locations: Metadata and GeoJSON features</h1>
         <p class="spectrum-Body spectrum-Body--M">GeoJSON features for all locations found in our dataset, useful for building map visualizations</p>
-        <!-- <sp-button variant="primary" quiet href="/map">View map</sp-button> -->
-        <sp-button emphasized href="${baseURL}features.json" target="_blank" download>Download</sp-button>
       </section>
     </div>
+
+      <hr>
+
+      <p class="spectrum-Body spectrum-Body--M">Documentation for the reports is available <a href="https://github.com/covidatlas/li/blob/master/docs/reports-v1.md">on GitHub.</a></p>
+
+      <h1>V1</h1>
+
+<h2>Latest</h2>
+<ul>${latestLinks}</ul>
+<h2>Timeseries</h2>
+<ul>${timeseriesLinks}</ul>
+<h2>Locations</h2>
+<ul>${locationLinks}</ul>
+
+</div>
+
     ${footer()}
   </div>
 </div>
